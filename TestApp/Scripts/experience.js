608f068a7a144e43b506e6f93ae7f532ac37b902
Justin Murray <33846034+syntheticmagus@users.noreply.github.com>
2019-09-23 11:22:25 -0700

    Reverted changes to experience.js

--- TestApp/Scripts/experience.js
+++ TestApp/Scripts/experience.js
@@ -124,22 +124,8 @@ CreateBoxAsync().then(function () {
         window.setTimeout(logFpsLoop, 3000);
     }
 
-    var xrPlugin = new XrPlugin(nativeEngine.getEngine());
-    xrPlugin.beginSession();
-
     engine.runRenderLoop(function () {
-        xrPlugin.beginFrame();
-        var activeFrameBuffers = xrPlugin.getActiveFrameBuffers();
-        if (activeFrameBuffers.length > 0) {
-            nativeEngine.bindFrameBuffer(activeFrameBuffers[0]);
-        }
-
         scene.render();
-
-        if (activeFrameBuffers.length > 0) {
-            nativeEngine.unbindFrameBuffer(activeFrameBuffers[0]);
-        }
-        xrPlugin.endFrame();
     });
 }, function (ex) {
     console.log(ex.message, ex.stack);
