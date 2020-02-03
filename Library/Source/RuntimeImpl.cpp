5a56eefee0ed6cf47f9cd9fd2efd34e24570b813
Justin Murray <33846034+syntheticmagus@users.noreply.github.com>
2020-02-03 13:11:16 -0800

    Super-preliminary v8 hacking.

--- Library/Source/RuntimeImpl.cpp
+++ Library/Source/RuntimeImpl.cpp
@@ -4,9 +4,14 @@
 #include "NativeWindow.h"
 #include "XMLHttpRequest.h"
 
+/*
 #define USE_EDGEMODE_JSRT
 #include <jsrt.h>
 #undef USE_EDGEMODE_JSRT
+*/
+
+#include <v8.h>
+#include <libplatform/libplatform.h>
 
 #include <curl/curl.h>
 #include <napi/env.h>
@@ -16,6 +21,9 @@ namespace Babylon
 {
     namespace
     {
+        using DispatchFunction = std::function<void(std::function<void()>)>;
+
+        /*
         void ThrowIfFailed(JsErrorCode errorCode)
         {
             if (errorCode != JsErrorCode::JsNoError)
@@ -24,7 +32,6 @@ namespace Babylon
             }
         }
 
-        using DispatchFunction = std::function<void(std::function<void()>)>;
         void CreateJsRuntimeForThread(DispatchFunction& dispatch)
         {
             // Create the runtime. We're only going to use one runtime for this host.
@@ -76,6 +83,74 @@ namespace Babylon
             // ThrowIfFailed(JsSetCurrentContext(JS_INVALID_REFERENCE));
             // 
             // ThrowIfFailed(JsDisposeRuntime(runtime));
+        }
+        */
+
+        class Module final
+        {
+        public:
+            Module(const char* executablePath)
+            {
+                v8::V8::InitializeICUDefaultLocation(executablePath);
+                v8::V8::InitializeExternalStartupData(executablePath);
+                m_platform = v8::platform::NewDefaultPlatform();
+                v8::V8::InitializePlatform(m_platform.get());
+                v8::V8::Initialize();
+            }
+
+            ~Module()
+            {
+                v8::V8::Dispose();
+                v8::V8::ShutdownPlatform();
+            }
+
+            static void Initialize(const char* executablePath)
+            {
+                if (s_module == nullptr)
+                {
+                    s_module = std::make_unique<Module>(executablePath);
+                }
+            }
+
+        private:
+            std::unique_ptr<v8::Platform> m_platform;
+
+            static std::unique_ptr<Module> s_module;
+        };
+
+        std::unique_ptr<Module> Module::s_module;
+
+        v8::Isolate* CreateIsolate(const char* executablePath)
+        {
+            Module::Initialize(executablePath);
+
+            v8::Isolate::CreateParams create_params;
+            create_params.array_buffer_allocator = v8::ArrayBuffer::Allocator::NewDefaultAllocator();
+            return v8::Isolate::New(create_params);
+        }
+
+        void DestroyIsolate(v8::Isolate* isolate)
+        {
+            // todo : GetArrayBufferAllocator not available?
+            //delete isolate->GetArrayBufferAllocator();
+            isolate->Dispose();
+        }
+
+        void CreateJsRuntimeForThread(v8::Isolate* isolate)
+        {
+            v8::Isolate::Scope isolate_scope{ isolate };
+            v8::HandleScope isolate_handle_scope{ isolate };
+            v8::Local<v8::Context> context = v8::Context::New(isolate);
+            v8::Context::Scope context_scope{ context };
+
+            auto env = Napi::Attach();
+
+            // Now we somehow have to do all of this stuff inside here, without ever exiting this scope. :-/
+        }
+
+        void DisposeJsRuntimeForThread()
+        {
+
         }
     }
 
