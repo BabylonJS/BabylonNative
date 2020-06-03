5552aad9cf72371734a0390f24dabf9d2c099e0a
syntheticmagus <33846034+syntheticmagus@users.noreply.github.com>
2020-06-03 14:38:06 -0700

    VERY preliminary shader manipulation using glslang.

--- Plugins/NativeEngine/Source/ShaderCompilerD3D.cpp
+++ Plugins/NativeEngine/Source/ShaderCompilerD3D.cpp
@@ -9,21 +9,290 @@
 #include <d3dcompiler.h>
 #include <wrl/client.h>
 
+#include <sstream>
+
 namespace Babylon
 {
+    namespace
+    {
+        using namespace glslang;
+
+        struct UniformToStructTraverser final : TIntermTraverser
+        {
+            UniformToStructTraverser()
+                : TIntermTraverser()
+            {
+            }
+
+            ~UniformToStructTraverser() override
+            {
+            }
+
+            void visitSymbol(TIntermSymbol* symbol) override
+            {
+                if (symbol->getType().getQualifier().isUniformOrBuffer())
+                {
+                    NameToSymbol[symbol->getName().c_str()] = symbol;
+                    SymbolsToParents.emplace_back(symbol, this->getParentNode());
+                }
+            }
+
+            static UniformToStructTraverser Traverse(glslang::TIntermediate* intermediate)
+            {
+                UniformToStructTraverser traverser{};
+                intermediate->getTreeRoot()->traverse(&traverser);
+
+                // Default loc we don't care about.
+                TSourceLoc loc{};
+
+                // Build the struct
+                std::vector<std::string> originalNames{};
+                // TODO: Leak less.
+                auto* structMembers = new TTypeList();
+
+                TPublicType publicType{};
+                publicType.qualifier.clearLayout();
+
+                for (const auto& [name, symbol] : traverser.NameToSymbol)
+                {
+                    const auto& type = symbol->getType();
+                    if (type.isMatrix())
+                    {
+                        publicType.setMatrix(type.getMatrixCols(), type.getMatrixRows());
+                    }
+                    else if (type.isVector())
+                    {
+                        publicType.setVector(type.getVectorSize());
+                    }
+
+                    originalNames.emplace_back(name);
+
+                    // TODO: Leak less.
+                    auto* newType = new TType(publicType);
+                    newType->setFieldName(name.c_str()); // TODO: Replace names here as necessary
+                    newType->setBasicType(symbol->getType().getBasicType());
+                    structMembers->emplace_back();
+                    structMembers->back().type = newType;
+                }
+
+                TQualifier qualifier{};
+                qualifier.clearLayout();
+                qualifier.storage = EvqUniform;
+                qualifier.layoutMatrix = ElmColumnMajor;
+                qualifier.layoutPacking = ElpStd140;
+                qualifier.layoutBinding = 0; // Determines which cbuffer it's bounds to (b0, b1, b2, etc.)
+
+                TType structType(structMembers, "Frame", qualifier);
+
+                TIntermSymbol* structSymbol = intermediate->addSymbol(structType, loc);
+
+                // Build the struct indexer operations.
+                std::map<std::string, TIntermBinary*> originalNameToReplacement{};
+                for (size_t idx = 0; idx < structMembers->size(); ++idx)
+                {
+                    auto* left = structSymbol;
+                    auto* right = intermediate->addConstantUnion(idx, loc);
+                    auto* binary = intermediate->addBinaryNode(EOpIndexDirectStruct, left, right, loc);
+                    originalNameToReplacement[originalNames[idx]] = binary;
+                }
+
+                for (const auto& [symbol, parent] : traverser.SymbolsToParents)
+                {
+                    auto* replacement = originalNameToReplacement[symbol->getName().c_str()];
+                    if (auto* aggregate = parent->getAsAggregate())
+                    {
+                        auto& sequence = aggregate->getSequence();
+                        for (int idx = 0; idx < sequence.size(); ++idx)
+                        {
+                            if (sequence[idx] == symbol)
+                            {
+                                sequence[idx] = replacement;
+                            }
+                        }
+                    }
+                    else if (auto* binary = parent->getAsBinaryNode())
+                    {
+                        if (binary->getLeft() == symbol)
+                        {
+                            binary->setLeft(replacement);
+                        }
+                        else
+                        {
+                            binary->setRight(replacement);
+                        }
+                    }
+                    else
+                    {
+                        throw std::runtime_error{"Cannot replace symbol: node type handler unimplemented"};
+                    }
+                }
+
+                return traverser;
+            }
+
+            std::map<std::string, TIntermSymbol*> NameToSymbol{};
+            std::vector<std::pair<TIntermSymbol*, TIntermNode*>> SymbolsToParents{};
+        };
+
+        class DebugTraverser : public TIntermTraverser
+        {
+        public:
+            DebugTraverser()
+                : TIntermTraverser{}
+            {
+            }
+
+            ~DebugTraverser() override
+            {
+            }
+
+            int doStuffNext = 0;
+            void visitSymbol(TIntermSymbol* symbol) override
+            {
+                if (doStuffNext == 0 && std::strcmp(symbol->getName().c_str(), "finalWorld") == 0)
+                {
+                    doStuffNext = 1;
+                }
+
+                Indent();
+                ss << "Symbol: " << symbol->getName() << std::endl;
+            }
+
+            void visitConstantUnion(TIntermConstantUnion* constUnion) override
+            {
+                Indent();
+                ss << "Constant Union: " << constUnion->getCompleteString().c_str() << std::endl;
+            }
+
+            bool visitBinary(TVisit, TIntermBinary* binary) override
+            {
+                if (binary->getOp() == glslang::EOpIndexDirectStruct && doStuffNext == 1)
+                {
+                    binary->setLeft(symbol);
+                    doStuffNext = 2;
+                }
+
+                Indent();
+                ss << "Binary: " << binary->getOp() << ", " << binary->getCompleteString().c_str() << std::endl;
+                return true;
+            }
+
+            bool visitUnary(TVisit, TIntermUnary* unary) override
+            {
+                Indent();
+                ss << "Unary: " << unary->getCompleteString().c_str() << std::endl;
+                return true;
+            }
+
+            bool visitSelection(TVisit, TIntermSelection* selection) override
+            {
+                Indent();
+                ss << "Selection: " << selection->getCompleteString().c_str() << std::endl;
+                return true;
+            }
+
+            bool visitAggregate(TVisit, TIntermAggregate* aggregate) override
+            {
+                Indent();
+                ss << "Aggregate: " << aggregate->getCompleteString().c_str() << std::endl;
+                return true;
+            }
+
+            bool visitLoop(TVisit, TIntermLoop* loop) override
+            {
+                Indent();
+                ss << "Loop: " << loop->getTest()->getCompleteString().c_str() << std::endl;
+                return true;
+            }
+
+            bool visitBranch(TVisit, TIntermBranch* branch) override
+            {
+                Indent();
+                ss << "Branch: " << branch->getExpression()->getCompleteString().c_str() << std::endl;
+                return true;
+            }
+
+            bool visitSwitch(TVisit, TIntermSwitch* _switch) override
+            {
+                Indent();
+                ss << "Switch: " << std::endl;
+                return true;
+            }
+
+            static std::string Traverse(glslang::TIntermediate* intermediate)
+            {
+                DebugTraverser traverser{};
+                traverser.DoStuff(intermediate);
+                intermediate->getTreeRoot()->traverse(&traverser);
+                return traverser.ss.str();
+            }
+
+        private:
+            std::stringstream ss{};
+
+            TIntermSymbol* symbol;
+            void DoStuff(TIntermediate* intermediate)
+            {
+                auto* structMembers = new TTypeList();
+
+                // Precursor type required for setting things that are otherwise inaccessible for some reason.
+                TPublicType pubMatrix{};
+                pubMatrix.qualifier.clearLayout();
+
+                pubMatrix.setMatrix(4, 4);
+                auto* matrix = new TType(pubMatrix);
+                matrix->setFieldName("fourByFour");
+                matrix->setBasicType(TBasicType::EbtFloat);
+                structMembers->emplace_back();
+                structMembers->back().type = matrix;
+
+                pubMatrix.setMatrix(3, 3);
+                auto* otherMatrix = new TType(pubMatrix);
+                otherMatrix->setFieldName("threeByThree");
+                otherMatrix->setBasicType(TBasicType::EbtFloat);
+                structMembers->emplace_back();
+                structMembers->back().type = otherMatrix;
+
+                pubMatrix.setVector(3);
+                auto* notMatrix = new TType(pubMatrix);
+                notMatrix->setFieldName("vector");
+                notMatrix->setBasicType(TBasicType::EbtFloat);
+                structMembers->emplace_back();
+                structMembers->back().type = notMatrix;
+
+                TQualifier qualifier{};
+                qualifier.clearLayout();
+                qualifier.storage = EvqUniform;
+                qualifier.layoutMatrix = ElmColumnMajor;
+                qualifier.layoutPacking = ElpStd140;
+                qualifier.layoutBinding = 0; // Determines which cbuffer it's bounds to (b0, b1, b2, etc.)
+
+                TType structType(structMembers, "Stuff", qualifier);
+
+                TSourceLoc loc{};
+                symbol = intermediate->addSymbol(structType, loc);
+            }
+
+            void Indent()
+            {
+                for (int idx = 0; idx < this->depth; ++idx)
+                {
+                    ss << "    ";
+                }
+            }
+        };
+    }
+
     namespace
     {
         void AddShader(glslang::TProgram& program, glslang::TShader& shader, std::string_view source)
         {
             const std::array<const char*, 1> sources{source.data()};
             shader.setStrings(sources.data(), gsl::narrow_cast<int>(sources.size()));
-            shader.setEnvInput(glslang::EShSourceGlsl, shader.getStage(), glslang::EShClientVulkan, 100);
-            shader.setEnvClient(glslang::EShClientVulkan, glslang::EShTargetVulkan_1_0);
-            shader.setEnvTarget(glslang::EShTargetSpv, glslang::EShTargetSpv_1_0);
-
-            if (!shader.parse(&DefaultTBuiltInResource, 450, false, EShMsgDefault))
+            
+            if (!shader.parse(&DefaultTBuiltInResource, 310, EProfile::EEsProfile, true, true, EShMsgDefault))
             {
-                throw std::exception(shader.getInfoDebugLog());
+                throw std::runtime_error(shader.getInfoDebugLog());
             }
 
             program.addShader(&shader);
@@ -85,11 +354,23 @@ namespace Babylon
         glslang::TShader fragmentShader{EShLangFragment};
         AddShader(program, fragmentShader, fragmentSource);
         InvertYDerivativeOperands(fragmentShader);
+        
+        // TODO: Do stuff here.
+        glslang::SpvVersion spv{};
+        spv.spv = 0x10000;
+        vertexShader.getIntermediate()->setSpv(spv);
+        fragmentShader.getIntermediate()->setSpv(spv);
 
         if (!program.link(EShMsgDefault))
         {
             throw std::exception(program.getInfoDebugLog());
         }
+
+        // auto vrtx = DebugTraverser::Traverse(program.getIntermediate(EShLangVertex));
+        // auto fgmt = DebugTraverser::Traverse(program.getIntermediate(EShLangFragment));
+
+        UniformToStructTraverser::Traverse(program.getIntermediate(EShLangVertex));
+        UniformToStructTraverser::Traverse(program.getIntermediate(EShLangFragment));
 
         // clang-format off
         static const spirv_cross::HLSLVertexAttributeRemap attributes[] = {
