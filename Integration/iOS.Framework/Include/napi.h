6c5c25073416d48b3397f6705ad489a1ac96ac98
Cedric Guillemet <cedric.guillemet@gmail.com>
2020-06-12 11:19:43 +0200

    Integration + iOS.Framework

---
+++ Integration/iOS.Framework/Include/napi.h
@@ -0,0 +1,15 @@
+#ifndef NAPI_H__
+#define NAPI_H__
+
+#define NODE_ADDON_API_DISABLE_NODE_SPECIFIC
+#define NODE_ADDON_API_DISABLE_DEPRECATED
+
+#include <BabylonNative/js_native_api_types.h>
+#include <BabylonNative/js_native_api.h>
+
+#include <BabylonNative/js_napi.h>
+#include <BabylonNative/napi-inl.h>
+
+#include <BabylonNative/env.h>
+
+#endif
---
+++ Integration/iOS.Framework/Source/framework.cpp
@@ -0,0 +1,1 @@
+// blank .cpp to bootstrap library creation
