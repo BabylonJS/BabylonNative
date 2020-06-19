05637cd6c18173cd42d9dac67561d161f1a476d2
Cedric Guillemet <cedric.guillemet@gmail.com>
2020-06-19 10:18:14 +0200

    removed empty line

--- Plugins/NativeEngine/Source/BgfxCallback.cpp
+++ Plugins/NativeEngine/Source/BgfxCallback.cpp
@@ -49,7 +49,6 @@ namespace Babylon
         }
         out[total] = '\0';
         bx::debugOutput(out);
-        
     }
 
     void BgfxCallback::profilerBegin(const char* /*name*/, uint32_t /*abgr*/, const char* /*filePath*/, uint16_t /*line*/)
