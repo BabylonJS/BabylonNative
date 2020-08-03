44d6e54e495ce0602be8c42b448713a53b8b8cb1
Alex Tran <altran@microsoft.com>
2020-08-03 15:22:16 -0700

    Disable feature points in the demo app by default.

--- Apps/Playground/Scripts/experience.js
+++ Apps/Playground/Scripts/experience.js
@@ -4,9 +4,9 @@ var logfps = true;
 var ibl = false;
 var rtt = false;
 var vr = false;
-var ar = true;
+var ar = false;
 var xrHitTest = false;
-var xrFeaturePoints = true;
+var xrFeaturePoints = false;
 var text = false;
 
 function CreateBoxAsync() {
