efd0eb97545f7410206dbf8630198f6558623462
Chris Barth <chriba@microsoft.com>
2020-09-10 16:28:41 -0700

    Add XrRegistry for accessing OpenXR components in plugins outside of xr component (#381)
    * base openxr off of openxr-mixedreality headers and add XrRegistry for accessing xr session data outside the plugin
    
    * cleanup content
    
    * add session state check
    
    * convert tabs to spaces
    
    * add xrInternal project and avoid exposing public headers
    
    * move to most recent version of mixed reality sdk
    
    * try changes to see if possible to see more compilation problems
    
    * enable platform specific extensions for openxr and allow xrplatform to be included more than once
    
    * try rearranging variable declaration to avoid warnings
    
    * avoid only using variable in assert
    
    * move towards using handles for xr cleanup
    
    * fix variable naming for public struct properties

---
+++ Dependencies/xr/Source/OpenXR/XrRegistry.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "XrSupportedExtensions.h"
+
+namespace xr
+{
+    struct XrSessionContext
+    {
+        XrSessionContext();
+        ~XrSessionContext();
+        bool IsInitialized() const;
+        XrInstance Instance() const;
+        XrSystemId SystemId() const;
+        XrTime DisplayTime() const;
+        const std::unique_ptr<XrSupportedExtensions>& Extensions() const;
+        const XrSession Session() const;
+        const XrSessionState State() const;
+        const XrSpace Space() const;
+
+        struct Impl;
+        std::unique_ptr<Impl> ContextImpl;
+    };
+
+    struct XrRegistry
+    {
+    public:
+        static const XrSessionContext& Context()
+        {
+            return s_context;
+        }
+
+    private:
+        static XrSessionContext s_context;
+    };
+}
