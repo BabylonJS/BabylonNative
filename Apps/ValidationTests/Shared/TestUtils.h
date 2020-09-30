9fd6f516ce87d1e5ba27c7f8b01e8a9daa96990e
Cedric Guillemet <cedric.guillemet@gmail.com>
2020-09-30 10:33:19 +0200

    revert back to use WN_DESTROY upload artifact from night build

--- Apps/ValidationTests/Shared/TestUtils.h
+++ Apps/ValidationTests/Shared/TestUtils.h
@@ -73,7 +73,7 @@ namespace Babylon
             doExit = true;
             errorCode = exitCode;
 #ifdef WIN32
-            PostMessageW((HWND)_nativeWindowPtr, WM_CLOSE, 0, 0);
+            PostMessageW((HWND)_nativeWindowPtr, WM_DESTROY, 0, 0);
 #elif __linux__
             Display* display = XOpenDisplay(NULL);
             XClientMessageEvent dummyEvent;
