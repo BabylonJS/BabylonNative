1fabb7cc8a6d48bac07b7bd891a032c9c44011c2
Ryan Tremblay <ryan.tremblay@microsoft.com>
2020-10-30 15:28:02 -0700

    Test again

--- Apps/ValidationTests/Scripts/validation_native.js
+++ Apps/ValidationTests/Scripts/validation_native.js
@@ -238,7 +238,7 @@ function runTest(index, done) {
                 BABYLON.Tools.BaseUrl = config.root + test.specificRoot;
             }
 
-//test6
+//test7
             console.log(`[RYANTREM] XHR 3`);
             var request = new XMLHttpRequest();
             request.open('GET', config.root + test.scriptToRun, true);
