4203519a61c38a743fa8fb6cc3b325bb8af3ec6b
Cedric Guillemet <cedric.guillemet@gmail.com>
2020-11-30 16:08:05 +0100

    ValidationTests IOS (#516)
    * ValidationTests for iOS
    
    * ios alert when finished testing
    
    Co-authored-by: Cedric Guillemet <ceguille@microsoft.com>

---
+++ Apps/ValidationTests/iOS/LibNativeBridge.mm
@@ -0,0 +1,76 @@
+#include "LibNativeBridge.h"
+
+#import <Babylon/AppRuntime.h>
+#import <Babylon/Graphics.h>
+#import <Babylon/ScriptLoader.h>
+#import <Babylon/Plugins/NativeEngine.h>
+#import <Babylon/Plugins/NativeXr.h>
+#import <Babylon/Polyfills/Window.h>
+#import <Babylon/Polyfills/XMLHttpRequest.h>
+#import <UIKit/UIKit.h>
+
+std::unique_ptr<Babylon::Graphics> graphics{};
+std::unique_ptr<Babylon::AppRuntime> runtime{};
+
+#import <Shared/TestUtils.h>
+
+@implementation LibNativeBridge
+
+- (instancetype)init
+{
+    self = [super init];
+    return self;
+}
+
+- (void)dealloc
+{
+}
+
+- (void)init:(void*)view width:(int)inWidth height:(int)inHeight
+{
+    runtime.reset();
+    graphics.reset();
+
+    float width = inWidth;
+    float height = inHeight;
+    void* windowPtr = view;
+    
+    graphics = Babylon::Graphics::CreateGraphics(windowPtr, static_cast<size_t>(width), static_cast<size_t>(height));
+    graphics->SetDiagnosticOutput([](const char* outputString) { printf("%s", outputString); fflush(stdout); });
+
+    runtime = std::make_unique<Babylon::AppRuntime>();
+
+    runtime->Dispatch([](Napi::Env env)
+    {
+        Babylon::Polyfills::Window::Initialize(env);
+        Babylon::Polyfills::XMLHttpRequest::Initialize(env);
+        
+        graphics->AddToJavaScript(env);
+        Babylon::Plugins::NativeEngine::Initialize(env);
+
+        // Initialize NativeXr plugin.
+        Babylon::Plugins::NativeXr::Initialize(env);
+        
+        Babylon::TestUtils::CreateInstance(env, nullptr);
+    });
+
+    Babylon::ScriptLoader loader{ *runtime };
+    loader.Eval("document = {}", "");
+    loader.LoadScript("app:///ammo.js");
+    loader.LoadScript("app:///recast.js");
+    loader.LoadScript("app:///babylon.max.js");
+    loader.LoadScript("app:///babylon.glTF2FileLoader.js");
+    loader.LoadScript("app:///babylonjs.materials.js");
+    loader.LoadScript("app:///babylon.gui.js");
+    loader.LoadScript("app:///validation_native.js");
+}
+
+- (void)resize:(int)inWidth height:(int)inHeight
+{
+    if (graphics)
+    {
+        graphics->UpdateSize(static_cast<size_t>(inWidth), static_cast<size_t>(inHeight));
+    }
+}
+
+@end
