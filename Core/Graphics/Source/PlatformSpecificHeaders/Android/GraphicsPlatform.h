8d337fa2105e9aa2bdba242feeee85d4fb0cbfe1
syntheticmagus <33846034+syntheticmagus@users.noreply.github.com>
2021-02-18 16:52:14 -0800

    Split GraphicsImpl into its own cpp file. Also created platform-specific headers to avoid ifdefs.

---
+++ Core/Graphics/Source/PlatformSpecificHeaders/Android/GraphicsPlatform.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <bgfx/bgfx.h>
+
+// MSAA is disabled on Android.
+// See issue https://github.com/BabylonJS/BabylonNative/issues/494#issuecomment-731135918
+// for explanation
+#define BGFX_RESET_FLAGS (BGFX_RESET_VSYNC | BGFX_RESET_MAXANISOTROPY)
