5e96a18829b7619de84a46d787255ba2abdf7284
rogerdis <rogerdis@microsoft.com>
2021-03-02 11:23:46 -0800

    Consolidate architecture logic

--- Dependencies/napi/napi-direct/source/env.cc
+++ Dependencies/napi/napi-direct/source/env.cc
@@ -5,7 +5,6 @@ namespace Napi
     Napi::Value Eval(Napi::Env env, const char* source, const char* sourceUrl)
     {
         napi_value result;
-
         NAPI_THROW_IF_FAILED(env, napi_run_script(env, Napi::String::New(env, source), sourceUrl, &result));
         return{ env, result };
     }
