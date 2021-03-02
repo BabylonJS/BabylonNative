4115940662d4eb168d18dfafce630fbcab67763a
Ryan Tremblay <ryan.tremblay@microsoft.com>
2021-03-02 10:10:30 -0800

    Use condition_variable

--- Core/Graphics/Source/SafeTimespanGuarantor.cpp
+++ Core/Graphics/Source/SafeTimespanGuarantor.cpp
@@ -1,38 +1,88 @@
 #include "SafeTimespanGuarantor.h"
 
+#include <thread>
+#include <chrono>
+
+using namespace std::chrono_literals;
+
 namespace Babylon
 {
     SafeTimespanGuarantor::SafeTimespanGuarantor()
-        : m_lock{m_mutex}
+        : m_lock{ m_mutex }
     {
     }
 
     void SafeTimespanGuarantor::BeginSafeTimespan()
     {
-        m_postCount = 0;
-        m_lock.reset();
+        if (m_begin)
+        {
+            throw std::runtime_error{"Already started"};
+        }
+        m_begin = true;
+        m_lock.unlock();
+        //std::this_thread::sleep_for(1ms);
+        std::this_thread::yield();
+        //m_postCount = 0;
+//        m_mutexes.clear();
+//        m_lock.reset();
     }
 
     void SafeTimespanGuarantor::EndSafeTimespan()
     {
-        bool wait{false};
-        do
+        if (!m_begin)
         {
-            m_lock.emplace(m_mutex);
-            wait = m_postCount > 0;
-            --m_postCount;
+            throw std::runtime_error{"Not started"};
+        }
+//        bool wait{false};
+//        do
+//        {
+//            m_lock.emplace(m_mutex);
+//            wait = m_postCount > 0;
+//            --m_postCount;
+//
+//            if (wait)
+//            {
+//                m_lock.reset();
+//            }
+//        } while (wait && m_semaphore.wait());
 
-            if (wait)
-            {
-                m_lock.reset();
-            }
-        } while (wait && m_semaphore.wait());
+//        m_lock.emplace(m_mutex);
+//        for (auto& mutex : m_mutexes)
+//        {
+//            //std::lock_guard<std::mutex> guard(*(mutex.get()));
+//            std::lock_guard<std::mutex> guard(mutex);
+//        }
+        
+        //std::unique_lock<std::mutex> lock(m_mutex);
+        m_lock.lock();
+        m_condition.wait(m_lock, [this]{ return m_count == 0; });
+        m_begin = false;
     }
 
     SafeTimespanGuarantor::SafetyGuarantee SafeTimespanGuarantor::GetSafetyGuarantee()
     {
-        std::scoped_lock lock{m_mutex};
-        ++m_postCount;
-        return m_semaphore.GetPostFinalAction();
+//        std::scoped_lock lock{m_mutex};
+//        ++m_postCount;
+//        return m_semaphore.GetPostFinalAction();
+        //std::lock_guard<std::mutex> guard(m_mutex);
+        //std::mutex mutex{};
+        //m_mutexes.push_back(std::mutex());
+        //m_mutexes.emplace_back();
+        //m_mutexes.push_back(std::unique_ptr<std::mutex>());
+        //m_mutexes.push_back({});
+//        m_mutexes.emplace_back();
+//        auto& mutex = m_mutexes.back();
+//        //return std::unique_lock<std::mutex>{ *(mutex->get()) };
+//        return std::unique_lock<std::mutex>(mutex);
+        std::lock_guard<std::mutex> guard(m_mutex);
+        m_count++;
+        return gsl::finally(std::function<void()>{ [this]()
+        {
+            {
+                std::unique_lock<std::mutex> lock(m_mutex);
+                m_count--;
+            }
+            m_condition.notify_one();
+        }});
     }
 }
--- Core/Graphics/Source/SafeTimespanGuarantor.h
+++ Core/Graphics/Source/SafeTimespanGuarantor.h
@@ -6,19 +6,21 @@
 
 #include <mutex>
 #include <optional>
+#include <vector>
+#include <deque>
 
 namespace Babylon
 {
-    class Semaphore : public bx::Semaphore
-    {
-    public:
-        auto GetPostFinalAction()
-        {
-            return gsl::finally([this]() {
-                post();
-            });
-        }
-    };
+//    class Semaphore : public bx::Semaphore
+//    {
+//    public:
+//        auto GetPostFinalAction()
+//        {
+//            return gsl::finally([this]() {
+//                post();
+//            });
+//        }
+//    };
 
     class SafeTimespanGuarantor
     {
@@ -28,13 +30,25 @@ namespace Babylon
         void BeginSafeTimespan();
         void EndSafeTimespan();
 
-        using SafetyGuarantee = decltype(std::declval<Semaphore>().GetPostFinalAction());
+        //using SafetyGuarantee = decltype(std::declval<Semaphore>().GetPostFinalAction());
+        //using SafetyGuarantee = std::unique_lock<std::mutex>;
+        using SafetyGuarantee = gsl::final_action<std::function<void()>>;
         SafetyGuarantee GetSafetyGuarantee();
 
     private:
-        Semaphore m_semaphore{};
-        size_t m_postCount{};
+//        Semaphore m_semaphore{};
+//        size_t m_postCount{};
+//        std::mutex m_mutex{};
+//        std::mutex m_mutex{};
+//        std::optional<std::scoped_lock<std::mutex>> m_lock{};
+//        //std::vector<std::unique_ptr<std::mutex>> m_mutexes{};
+//        std::deque<std::mutex> m_mutexes{};
+        
+//        std::atomic<uint32_t> m_count{};
+        uint32_t m_count{};
         std::mutex m_mutex{};
-        std::optional<std::scoped_lock<std::mutex>> m_lock{};
+        std::unique_lock<std::mutex> m_lock{};
+        std::condition_variable m_condition{};
+        bool m_begin{};
     };
 }
