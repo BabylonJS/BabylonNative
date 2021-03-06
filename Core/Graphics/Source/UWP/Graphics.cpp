a549344ed3766c996774595de00dda5b825174eb
Nicholas Barlow <whoisdrigax@gmail.com>
2021-03-06 04:53:30 -0800

    Implement Unix X11 based implementation, add mutex to UWP implementation

--- Core/Graphics/Source/UWP/Graphics.cpp
+++ Core/Graphics/Source/UWP/Graphics.cpp
@@ -7,13 +7,13 @@ namespace Babylon
     {
         // In windows, 100% DPI scaling is 96dpi.
         // See https://docs.microsoft.com/en-us/windows/win32/learnwin32/dpi-and-device-independent-pixels
-        
+
         // In UWP, DisplayInformation is only accessible from the UI thread.
         // See https://docs.microsoft.com/en-us/uwp/api/windows.graphics.display.displayinformation.getforcurrentview?view=winrt-19041#Windows_Graphics_Display_DisplayInformation_GetForCurrentView
-        
+
         // TODO: We need to pass a Windows::UI::Core::CoreDispatcher to the graphics component so that we can safely make UI calls.
         // See https://github.com/BabylonJS/BabylonNative/issues/625
-        
+        std::scoped_lock lock{m_state.Mutex};
         return m_state.Resolution.DevicePixelRatio;
     }
 }
---
+++ Core/Graphics/Source/Unix/Graphics.cpp
@@ -0,0 +1,33 @@
+#include <Babylon/Graphics.h>
+#include "../GraphicsImpl.h"
+
+#include <X11/Xlib.h>
+
+constexpr float MILLIMETERS_TO_INCHES = 0.03937;
+
+namespace Babylon
+{
+    float Graphics::Impl::UpdateDevicePixelRatio()
+    {
+        // TODO: We should persist a Display object instead of opening a new display.
+        // See https://github.com/BabylonJS/BabylonNative/issues/625
+
+        auto display = XOpenDisplay(nullptr);
+        auto screen = DefaultScreen(display);
+
+        auto width = DisplayWidthMM(display, screen);
+        auto pixelWidth = DisplayWidth(display, screen);
+
+        std::scoped_lock lock{m_state.Mutex};
+        if (width > 0){
+            auto dpi = pixelWidth/(width * MILLIMETERS_TO_INCHES);
+
+            // X11 does not enforce a default dpi.
+            // Use 96 dpi as our baseline DPI to match the behavior of Windows, and the default behavior of Linux Desktop Environments such as Gnome and KDE.
+            // See: https://scanline.ca/dpi/
+            m_state.Resolution.DevicePixelRatio = dpi/96.0f;
+        }
+
+        return m_state.Resolution.DevicePixelRatio;
+    }
+}
