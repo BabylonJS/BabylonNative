d14426f8edad9edf1f6de1815322661ef54dc92b
Ryan Tremblay <ryan.tremblay@microsoft.com>
2021-03-18 11:08:42 -0700

    Disable vsync on Android since we are currently rendering on the main thread which is already throttled (#642)

--- Core/Graphics/Source/Android/GraphicsPlatform.h
+++ Core/Graphics/Source/Android/GraphicsPlatform.h
@@ -5,7 +5,7 @@
 // MSAA is disabled on Android.
 // See issue https://github.com/BabylonJS/BabylonNative/issues/494#issuecomment-731135918
 // for explanation
-#define BGFX_RESET_FLAGS (BGFX_RESET_VSYNC | BGFX_RESET_MAXANISOTROPY)
+#define BGFX_RESET_FLAGS (BGFX_RESET_MAXANISOTROPY)
 
 namespace Babylon
 {
