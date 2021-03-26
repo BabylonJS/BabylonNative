0a8bc4c7756be692157a4f51573235c1dafcf1ee
Ryan Tremblay <ryan.tremblay@microsoft.com>
2021-03-26 10:02:57 -0700

    Cleanup

--- Dependencies/xr/Source/ARCore/XR.cpp
+++ Dependencies/xr/Source/ARCore/XR.cpp
@@ -330,7 +330,6 @@ namespace xr
 
     struct System::Session::Impl
     {
-        using EGLContextPtr = std::unique_ptr<std::remove_pointer_t<EGLContext>, std::function<void(EGLContext)>>;
         using EGLSurfacePtr = std::unique_ptr<std::remove_pointer_t<EGLSurface>, std::function<void(EGLSurface)>>;
 
         const System::Impl& SystemImpl;
@@ -347,7 +346,7 @@ namespace xr
         Impl(System::Impl& systemImpl, void* graphicsContext, std::function<void*()> windowProvider)
             : SystemImpl{ systemImpl }
             , windowProvider{ [windowProvider{ std::move(windowProvider) }] { return reinterpret_cast<ANativeWindow*>(windowProvider()); } }
-            , parentContext{reinterpret_cast<EGLContext>(graphicsContext) }
+            , context{reinterpret_cast<EGLContext>(graphicsContext) }
             , pauseTicket{AddPauseCallback([this]() { this->PauseSession(); }) }
             , resumeTicket{AddResumeCallback([this]() { this->ResumeSession(); }) }
         {
@@ -402,8 +401,6 @@ namespace xr
                 {
                     throw std::runtime_error{"Failed to choose EGL config."};
                 }
-
-                eglGetConfigAttrib(display, config, EGL_NATIVE_VISUAL_ID, &format);
             }
 
             // Generate a texture id for the camera texture (ARCore will allocate the texture itself)
@@ -645,8 +642,7 @@ namespace xr
             ArFrame_getTimestamp(session, frame, &frameTimestamp);
             if (frameTimestamp && surface.get())
             {
-                //auto surfaceTransaction{ GLTransactions::MakeCurrent(eglGetDisplay(EGL_DEFAULT_DISPLAY), surface.get(), surface.get(), context.get()) };
-                auto surfaceTransaction{ GLTransactions::MakeCurrent(eglGetDisplay(EGL_DEFAULT_DISPLAY), surface.get(), surface.get(), parentContext) };
+                auto surfaceTransaction{ GLTransactions::MakeCurrent(eglGetDisplay(EGL_DEFAULT_DISPLAY), surface.get(), surface.get(), context) };
 
                 auto bindFrameBufferTransaction{ GLTransactions::BindFrameBuffer(0) };
                 auto cullFaceTransaction{ GLTransactions::SetCapability(GL_CULL_FACE, false) };
@@ -1047,8 +1043,7 @@ namespace xr
         EGLDisplay display{};
         EGLConfig config{};
         EGLint format{};
-        EGLContext parentContext{};
-        //EGLContextPtr context;
+        EGLContext context{};
         EGLSurfacePtr surface;
 
         GLuint cameraShaderProgramId{};
