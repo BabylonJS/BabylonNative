b3acc58e2ea2feff7ed9567d2083ef583a35fe85
Chris Barth <chriba@microsoft.com>
2021-04-12 10:20:56 -0700

    move content to new pipeline definition

---
+++ Apps/BabylonScripts/UpdateScripts.ps1
@@ -0,0 +1,12 @@
+Push-Location $PSScriptRoot
+Write-Host "Installing npm packages..."
+npm install
+
+$files = "babylon.max.js", "babylon.gui.js", "babylonjs.materials.js"
+foreach ($file in $files)
+{
+    $localFile = "$PSScriptRoot\$file"
+    $fileInPackage = Get-ChildItem "$PSScriptRoot\node_modules" -Filter $file -Recurse | % { $_.FullName }
+    Write-Host "Copying $fileInPackage to $localFile"
+    Copy-Item -Path $fileInPackage -Destination $localFile -Force
+}
