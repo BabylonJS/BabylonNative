d9d45de654c68027c1cceb64eec950a28cb075fb
rgerd <rgerd@users.noreply.github.com>
2021-04-14 13:33:54 -0700

    Chrome DevTools support for Win32 x86/x64 (#607)

---
+++ Dependencies/v8inspector/Include/V8Inc.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifndef __clang__
+#pragma warning(push)
+#pragma warning(disable: 4100 4267 4127)
+#endif
+
+#include <v8.h>
+#include <v8-inspector.h>
+#include <libplatform/libplatform.h>
+
+#ifndef __clang__
+#pragma warning(pop)
+#endif
