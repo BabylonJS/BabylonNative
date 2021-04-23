42cb2e83849953a1087d04ed45f72dccc7b27a62
Cedric Guillemet <cedric.guillemet@gmail.com>
2021-04-23 11:25:51 +0200

    Create Graphics from a native Context (#674)
    * ContextType type and function definitions
    
    * context struct
    
    * fix context includes
    
    * DevicePixelRatio
    
    * PR feedback
    
    * warning message on API subject to change
    
    Co-authored-by: Cedric Guillemet <ceguille@microsoft.com>

--- Core/Graphics/Include/UWP/CoreWindow/Babylon/GraphicsPlatform.h
+++ Core/Graphics/Include/UWP/CoreWindow/Babylon/GraphicsPlatform.h
@@ -3,15 +3,23 @@
 #include <windows.ui.core.h>
 #include <winrt/windows.ui.core.h>
 #include <winrt/Windows.ApplicationModel.h>
+#include <d3d11.h>
 
 namespace Babylon
 {
     using WindowType = winrt::Windows::UI::Core::ICoreWindow*;
+    using ContextType = ID3D11DeviceContext*;
 
-    struct GraphicsConfiguration
+    struct WindowConfiguration
     {
         WindowType WindowPtr;
         size_t Width;
         size_t Height;
+    };
+
+    struct ContextConfiguration
+    {
+        ContextType Context;
+        float DevicePixelRatio{1.f};
     };
 }
