5c07e4f2280e6ecfca64865d5893f39ba647ccf0
Ryan Tremblay <ryan.tremblay@microsoft.com>
2021-06-11 10:40:13 -0700

    Add comment

--- Core/AppRuntime/Include/Babylon/AppRuntime.h
+++ Core/AppRuntime/Include/Babylon/AppRuntime.h
@@ -36,6 +36,9 @@ namespace Babylon
         void RunPlatformTier();
         void RunEnvironmentTier(const char* executablePath = ".");
         void Run(Napi::Env);
+
+        // This method is called from Dispatch to allow platform-specific code to add
+        // extra logic around the invocation of a dispatched callback.
         void Execute(std::function<void()> callback);
 
         static void DefaultUnhandledExceptionHandler(std::exception_ptr ptr);
