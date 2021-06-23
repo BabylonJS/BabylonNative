7aa1efa3bbbc42cc82eebc4f9b06f335c24b2786
Darragh Burke <darraghjburke@gmail.com>
2021-06-23 13:50:58 -0600

    added comments

--- Apps/UnitTests/UnitTests.cpp
+++ Apps/UnitTests/UnitTests.cpp
@@ -5,7 +5,9 @@
 
 namespace Babylon
 {
+    // Waits for an arcana task to complete before continuing
     void WaitForCompletion(arcana::task<void, std::exception_ptr> task) {
+        // The boolean flag is a temporary hack that should be replaced by proper synchronization code
         bool done{false};
         task.then(arcana::inline_scheduler, arcana::cancellation::none(), [&done](arcana::expected<void, std::exception_ptr> result) {
             done = true;
