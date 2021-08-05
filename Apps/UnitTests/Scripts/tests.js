fec9427562c62a7c6fd7f9bc1591af92b176d426
Darragh Burke <darraghjburke@gmail.com>
2021-08-05 15:18:15 -0700

    make string literals more consistent

--- Apps/UnitTests/Scripts/tests.js
+++ Apps/UnitTests/Scripts/tests.js
@@ -2,20 +2,20 @@
 function BabylonReporter(runner) {
     const stats = runner.stats;
 
-    runner.on('pass', function (test) {
+    runner.on("pass", function (test) {
         console.log(`Passed: ${test.fullTitle()}`);
     });
 
-    runner.on('fail', function (test, err) {
+    runner.on("fail", function (test, err) {
         console.log(`Failed: ${test.fullTitle()} with error: ${err.message}`);
     });
 
-    runner.on('end', function () {
+    runner.on("end", function () {
         console.log(`Total passed: ${stats.passes}/${stats.tests} tests`);
     });
 }
 
-mocha.setup({ ui: 'bdd', reporter: BabylonReporter });
+mocha.setup({ ui: "bdd", reporter: BabylonReporter });
 
 function makeXHR(method, url) {
     return new Promise( (resolve, reject) => {
@@ -28,18 +28,18 @@ function makeXHR(method, url) {
 
 const expect = chai.expect;
 
-describe('XMLHTTPRequest', function () {
+describe("XMLHTTPRequest", function () {
     this.timeout(0);
-    it('should have readyState=4 when load ends', async function () {
+    it("should have readyState=4 when load ends", async function () {
         const xhr = await makeXHR("GET", "https://babylonjs.com");
         expect(xhr.readyState).to.equal(4);
     })
-    it('should have status=200 for a valid page', async function () {
+    it("should have status=200 for a valid page", async function () {
         const xhr = await makeXHR("GET", "https://babylonjs.com");
         expect(xhr.status).to.equal(200);
     })
-    it('should have status=404 for a page that does not exist', async function () {
-        const xhr = await makeXHR('GET', 'https://babylonjs.com/invalid');
+    it("should have status=404 for a page that does not exist", async function () {
+        const xhr = await makeXHR("GET", "https://babylonjs.com/invalid");
         expect(xhr.status).to.equal(404);
     })
 
