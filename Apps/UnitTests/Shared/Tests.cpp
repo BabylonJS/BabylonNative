3908e9b717c301a801a519df3d7f658ee97ccab9
Darragh Burke <darraghjburke@gmail.com>
2021-09-23 13:13:02 -0700

    XHR: Make open() throw synchronous errors for bad URLs (#872)
    * replace GoogleTest tests with JS tests using Mocha and Chai
    
    * make string literals more consistent
    
    * clean  up changes
    
    * fix spacing
    
    * create doc pages
    
    * patch android and windows, add tests
    
    * throw if user calls send() before opening
    
    * move curl handle creation to open
    
    * xhr open throws + unit tests on macos
    
    * fix spacing and brackets
    
    * use c++ exception rather than ThrowAsJS
    
    * Fix napi jsc exception handling
    
    * set status code even if not 200 on unix
    
    * port unit tests to unix and move chai+mocha to npm
    
    * nits
    
    * bracket on new line
    
    * let tests.js set the exit code for the program
    
    * build and run unit tests on macos in CI
    
    * fix build path
    
    * fix yml syntax
    
    * run linux and mac tests
    
    * fix linux spacing
    
    * add more unix builds and fix mac
    
    * unit tests win32
    
    * cleanup runtime
    
    * test only windows
    
    * fix jsi crash and enable CI
    
    * fix dependencies + JSI NAPI bug workaround
    
    * address pr comments
    
    * catch unknown exceptions
    
    * address comments
    
    * fix url name conflict unix
    
    * unix fix curl url object name
    
    * fix package.json
    
    Co-authored-by: Gary Hsu <bghgary@users.noreply.github.com>

--- Apps/UnitTests/Shared/Tests.cpp
+++ Apps/UnitTests/Shared/Tests.cpp
@@ -1,4 +1,3 @@
-#pragma once
 #include <UrlLib/UrlLib.h>
 #include <arcana/threading/task.h>
 #include <future>
@@ -11,35 +10,56 @@
 #include <Babylon/ScriptLoader.h>
 #include <chrono>
 #include <thread>
+#include <napi/env.h>
 
 std::unique_ptr<Babylon::AppRuntime> runtime{};
 std::unique_ptr<Babylon::Graphics> graphics{};
 
-int main()
+std::promise<int32_t> exitCode;
+
+static inline constexpr const char* JS_FUNCTION_NAME{ "SetExitCode" };
+void SetExitCode(const Napi::CallbackInfo& info)
+{
+    Napi::Env env = info.Env();
+    if (info.Length() < 1)
+    {
+        throw Napi::TypeError::New(env, "Must provide an exit code");
+    }
+    exitCode.set_value(info[0].As<Napi::Number>().Int32Value());
+}
+
+int run()
 {
     Babylon::ContextConfiguration graphicsConfig{};
     graphics = Babylon::Graphics::CreateGraphics(graphicsConfig);
 
     runtime = std::make_unique<Babylon::AppRuntime>();
-    runtime->Dispatch([](Napi::Env env) {
+    runtime->Dispatch([](Napi::Env env)
+    {
         graphics->AddToJavaScript(env);
 
         Babylon::Polyfills::XMLHttpRequest::Initialize(env);
-        Babylon::Polyfills::Console::Initialize(env, [](const char* message, auto) {
+        Babylon::Polyfills::Console::Initialize(env, [](const char* message, auto)
+        {
             printf("%s", message);
             fflush(stdout);
         });
         Babylon::Polyfills::Window::Initialize(env);
         Babylon::Plugins::NativeEngine::Initialize(env);
+        
+        env.Global().Set(JS_FUNCTION_NAME, Napi::Function::New(env, SetExitCode, JS_FUNCTION_NAME));
     });
     Babylon::ScriptLoader loader{*runtime};
     loader.Eval("global = {};", ""); // Required for Chai.js as we do not have global in Babylon Native
     loader.Eval("window.clearTimeout = () => {};", ""); // TODO: implement clear timeout, required for Mocha timeouts to work correctly
     loader.Eval("location = {href: ''};", "");          // Required for Mocha.js as we do not have a location in Babylon Native
     loader.LoadScript("app:///Scripts/babylon.max.js");
-    loader.LoadScript("https://unpkg.com/chai/chai.js");
-    loader.LoadScript("https://unpkg.com/mocha/mocha.js");
+    loader.LoadScript("app:///Scripts/chai.js");
+    loader.LoadScript("app:///Scripts/mocha.js");
     loader.LoadScript("app:///Scripts/tests.js");
-    // TODO: add mechanism to wait until an exit code is received from tests.js
-    std::this_thread::sleep_for(std::chrono::seconds(1000000));
+    
+    auto code{exitCode.get_future().get()};
+    runtime.reset();
+    graphics.reset();
+    return code;
 }
