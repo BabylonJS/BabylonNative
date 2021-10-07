ea76524323cbe6ad3f8e3cb6499830eb850d2686
Justin Murray <33846034+syntheticmagus@users.noreply.github.com>
2021-10-07 14:11:38 -0700

    Simplify Work Queue (#882)
    * Attempt to simplify work queue to make handling errors a bit easier.
    
    * PR comments and fix for potential leak.

--- Core/AppRuntime/Source/WorkQueue.h
+++ Core/AppRuntime/Source/WorkQueue.h
@@ -19,17 +19,20 @@ namespace Babylon
         template<typename CallableT>
         void Append(CallableT callable)
         {
-            std::scoped_lock lock{m_appendMutex};
-            m_task = m_task.then(m_dispatcher, m_cancelSource, [this, callable = std::move(callable)]() mutable noexcept {
-                try
-                {
-                    callable(m_env.value());
-                }
-                catch (...)
-                {
-                    m_unhandledExceptionHandler(std::current_exception());
-                }
-            });
+            // Manual dispatcher queueing requires a copyable CallableT, we use a shared pointer trick to make a 
+            // copyable callable if necessary.
+            if constexpr (std::is_copy_constructible<CallableT>::value)
+            {
+                m_dispatcher.queue([this, callable = std::move(callable)]() {
+                    Invoke(callable);
+                });
+            }
+            else
+            {
+                m_dispatcher.queue([this, callablePtr = std::make_shared<CallableT>(std::move(callable))]() {
+                    Invoke(*callablePtr);
+                });
+            }
         }
 
         void Suspend();
@@ -37,14 +40,24 @@ namespace Babylon
         void Run(Napi::Env);
 
     private:
-        std::optional<Napi::Env> m_env{};
+        template<typename CallableT>
+        void Invoke(CallableT& callable)
+        {
+            try
+            {
+                callable(m_env.value());
+            }
+            catch (...)
+            {
+                m_unhandledExceptionHandler(std::current_exception());
+            }
+        }
 
-        std::mutex m_appendMutex{};
+        std::optional<Napi::Env> m_env{};
 
         std::optional<std::scoped_lock<std::mutex>> m_suspensionLock{};
 
         arcana::cancellation_source m_cancelSource{};
-        arcana::task<void, std::error_code> m_task = arcana::task_from_result<std::error_code>();
         arcana::manual_dispatcher<128> m_dispatcher{};
 
         std::thread m_thread;
