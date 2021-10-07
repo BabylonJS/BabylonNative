342d56de5818d430a7c2e1e31eaa2d5ba78926eb
Gary Hsu <bghgary@users.noreply.github.com>
2021-10-07 10:05:39 -0700

    Factor index and vertex buffers and fix load perf (#905)

---
+++ Plugins/NativeEngine/Source/VertexArray.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "IndexBuffer.h"
+#include "VertexBuffer.h"
+#include <map>
+
+namespace Babylon
+{
+    class VertexArray final
+    {
+    public:
+        ~VertexArray();
+
+        void RecordIndexBuffer(IndexBuffer* indexBuffer);
+        void RecordVertexBuffer(VertexBuffer* vertexBuffer, uint32_t location, uint32_t byteOffset, uint32_t byteStride, uint32_t numElements, uint32_t type, bool normalized);
+        void SetIndexBuffer(bgfx::Encoder* encoder, uint32_t firstIndex, uint32_t numIndices);
+        void SetVertexBuffers(bgfx::Encoder* encoder, uint32_t startVertex, uint32_t numVertices);
+
+    private:
+        struct IndexBufferRecord
+        {
+            IndexBuffer* Buffer{};
+        };
+
+        IndexBufferRecord m_indexBufferRecord;
+
+        struct VertexBufferRecord
+        {
+            VertexBuffer* Buffer{};
+            uint32_t Offset{};
+            bgfx::VertexLayoutHandle LayoutHandle{};
+        };
+
+        std::map<bgfx::Attrib::Enum, VertexBufferRecord> m_vertexBufferRecords;
+    };
+}
