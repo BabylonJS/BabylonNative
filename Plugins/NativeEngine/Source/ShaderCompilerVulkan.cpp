d5344f32bbbea9e2be5e8212457405ab145216f5
Cedric Guillemet <ceguille@microsoft.com>
2021-11-19 18:19:14 +0100

    vulkan shader location

--- Plugins/NativeEngine/Source/ShaderCompilerVulkan.cpp
+++ Plugins/NativeEngine/Source/ShaderCompilerVulkan.cpp
@@ -79,7 +79,7 @@ namespace Babylon
         auto cutScope = ShaderCompilerTraversers::ChangeUniformTypes(program, ids);
         auto utstScope = ShaderCompilerTraversers::MoveNonSamplerUniformsIntoStruct(program, ids);
         std::unordered_map<std::string, std::string> vertexAttributeRenaming = {};
-        ShaderCompilerTraversers::AssignLocationsAndNamesToVertexVaryings(program, ids, vertexAttributeRenaming);
+        ShaderCompilerTraversers::AssignLocationsAndNamesToVertexVaryingsD3D(program, ids, vertexAttributeRenaming);
         ShaderCompilerTraversers::SplitSamplersIntoSamplersAndTextures(program, ids);
         ShaderCompilerTraversers::InvertYDerivativeOperands(program);
 
