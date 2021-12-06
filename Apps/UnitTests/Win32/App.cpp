e4e8614035759387a1a20b6f4f21d518edcf893d
Cedric Guillemet <ceguille@microsoft.com>
2021-12-06 15:57:48 +0100

    hwnd for win32 unittests

--- Apps/UnitTests/Win32/App.cpp
+++ Apps/UnitTests/Win32/App.cpp
@@ -1,6 +1,18 @@
 #include "../Shared/Tests.cpp"
 
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    return ::DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int main() {
-    Babylon::ContextConfiguration graphicsConfig{};
-    return Run(Babylon::Graphics::CreateGraphics(graphicsConfig));
+    WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, "BabylonNative", NULL };
+    ::RegisterClassEx(&wc);
+    HWND hwnd = ::CreateWindow(wc.lpszClassName, "BabylonNative", WS_OVERLAPPEDWINDOW, -1, -1, -1, -1, NULL, NULL, wc.hInstance, NULL);
+
+    Babylon::WindowConfiguration windowConfig{};
+    windowConfig.WindowPtr = hwnd;
+    windowConfig.Width = 600;
+    windowConfig.Height = 400;
+    return Run(Babylon::Graphics::CreateGraphics(windowConfig));
 }
