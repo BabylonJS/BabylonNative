60b37e2885d286691d35033ad26aa4ea34bd5214
rgerd <rgerd@users.noreply.github.com>
2021-12-17 14:50:56 -0800

    Define XRWebGLBinding (#964)

--- Plugins/NativeXr/Source/NativeXr.cpp
+++ Plugins/NativeXr/Source/NativeXr.cpp
@@ -854,6 +854,35 @@ namespace Babylon
             }
         };
 
+        class XRWebGLBinding : public Napi::ObjectWrap<XRWebGLBinding>
+        {
+            static constexpr auto JS_CLASS_NAME = "XRWebGLBinding";
+
+        public:
+            static void Initialize(Napi::Env env)
+            {
+                Napi::HandleScope scope{env};
+
+                Napi::Function func = DefineClass(
+                    env,
+                    JS_CLASS_NAME,
+                    {
+                    });
+
+                env.Global().Set(JS_CLASS_NAME, func);
+            }
+
+            static Napi::Object New(const Napi::CallbackInfo& info)
+            {
+                return info.Env().Global().Get(JS_CLASS_NAME).As<Napi::Function>().New({});
+            }
+
+            XRWebGLBinding(const Napi::CallbackInfo& info)
+                : Napi::ObjectWrap<XRWebGLBinding>{info}
+            {
+            }
+        };
+
         class XRWebGLLayer : public Napi::ObjectWrap<XRWebGLLayer>
         {
             static constexpr auto JS_CLASS_NAME = "XRWebGLLayer";
@@ -3342,6 +3371,7 @@ namespace Babylon
 
             PointerEvent::Initialize(env);
 
+            XRWebGLBinding::Initialize(env);
             XRWebGLLayer::Initialize(env);
             XRRigidTransform::Initialize(env);
             XRView::Initialize(env);
