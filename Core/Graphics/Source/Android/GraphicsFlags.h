6071267b939bdd0fae657613fe85acc370ab578e
Cedric Guillemet <ceguille@microsoft.com>
2022-01-17 11:18:55 +0100

    no msaa Android

--- Core/Graphics/Source/Android/GraphicsFlags.h
+++ Core/Graphics/Source/Android/GraphicsFlags.h
@@ -2,4 +2,6 @@
 
 #include <bgfx/bgfx.h>
 
-constexpr uint32_t BGFX_RESET_FLAGS = BGFX_RESET_VSYNC | BGFX_RESET_MSAA_X4 | BGFX_RESET_MAXANISOTROPY;
+// MSAA is disabled on Android
+// more infos here : https://github.com/BabylonJS/BabylonNative/issues/507
+constexpr uint32_t BGFX_RESET_FLAGS = BGFX_RESET_VSYNC /*| BGFX_RESET_MSAA_X4*/ | BGFX_RESET_MAXANISOTROPY;
