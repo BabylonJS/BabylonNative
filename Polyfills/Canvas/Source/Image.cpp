4848557430e71d4acfe8ce1c8be345d66c4f9b86
Alex Tran <altran@microsoft.com>
2022-03-14 14:33:38 -0700

    Support NativeCanvas images in createImageBitmap (#1021)
    * Initial implementation for Image.decode and supporting Image object in createImageBitmap
    
    * Fix propagation of image container.
    
    * Remove global image polyfill.
    
    * Remove unneeded decode method, and add onerror callback
    
    * Fully remove decode implementation
    
    * Add safety check for m_onloadHandlerRef
    
    * Remove lambda capture for thisObject.
    
    * Change null checks to Napi IsEmpty calls.
    
    * Add better handling for the case where we get an empty buffer, but no HTTP Request error
    
    * Address PR Comments
    
    * One more error handler
    
    * Remove m_jsThis persistent ref, and add helper function for HandleLoadImageError.
    
    * this->Value() to Value() for UWP/Windows build.
    
    * Make ObjectWrap extend Reference<Object> for Napi-jsi
    
    * Address PR comments
    
    * Add napi_env back to initializer
    
    * Change _imageContainer to an instance accessor instead of using Value().

--- Polyfills/Canvas/Source/Image.cpp
+++ Polyfills/Canvas/Source/Image.cpp
@@ -11,6 +11,7 @@
 #include <bimg/decode.h>
 #include "nanovg.h"
 #include <cassert>
+#include <napi/napi_pointer.h>
 
 namespace Babylon::Polyfills::Internal
 {
@@ -30,6 +31,9 @@ namespace Babylon::Polyfills::Internal
                 InstanceAccessor("naturalHeight", &NativeCanvasImage::GetNaturalHeight, nullptr),
                 InstanceAccessor("src", &NativeCanvasImage::GetSrc, &NativeCanvasImage::SetSrc),
                 InstanceAccessor("onload", nullptr, &NativeCanvasImage::SetOnload),
+                InstanceAccessor("onerror", nullptr, &NativeCanvasImage::SetOnerror),
+                // TODO: This should be set directly on the JS Object rather than via an instanceAccessor see: https://github.com/BabylonJS/BabylonNative/issues/1030
+                InstanceAccessor("_imageContainer", &NativeCanvasImage::GetImageContainer, nullptr),
             });
 
         JsRuntime::NativeObject::GetFromJavaScript(env).Set(JS_CONSTRUCTOR_NAME, func);
@@ -82,6 +86,18 @@ namespace Babylon::Polyfills::Internal
         return Napi::Value::From(Env(), m_src);
     }
 
+    Napi::Value NativeCanvasImage::GetImageContainer(const Napi::CallbackInfo&)
+    {
+        if (m_imageContainer != nullptr)
+        {
+            return Napi::Pointer<bimg::ImageContainer>::Create(Env(), m_imageContainer);
+        }
+        else
+        {
+            return Env().Null();
+        }
+    }
+
     void NativeCanvasImage::SetSrc(const Napi::CallbackInfo& info, const Napi::Value& value)
     {
         auto text{value.As<Napi::String>().Utf8Value()};
@@ -91,22 +107,33 @@ namespace Babylon::Polyfills::Internal
         request.SendAsync().then(m_runtimeScheduler, *m_cancellationSource, [env{info.Env()}, this, request{std::move(request)}](arcana::expected<void, std::exception_ptr> result) {
             if (result.has_error())
             {
-                throw Napi::Error::New(env, result.error());
+                HandleLoadImageError(Napi::Error::New(env, result.error()));
+                return;
             }
 
             Dispose();
 
             auto buffer{request.ResponseBuffer()};
+            if (buffer.data() == nullptr || buffer.size_bytes() == 0)
+            {
+                HandleLoadImageError(Napi::Error::New(env, "Image with provided source returned empty response."));
+                return;
+            }
+
             m_imageContainer = bimg::imageParse(&m_allocator, buffer.data(), static_cast<uint32_t>(buffer.size_bytes()));
             if (m_imageContainer == nullptr)
             {
-                Napi::Error::New(env, "Unable to decode image with provided src for in Canvas.").ThrowAsJavaScriptException();
+                HandleLoadImageError(Napi::Error::New(env, "Unable to decode image with provided src."));
+                return;
             }
 
             m_width = m_imageContainer->m_width;
             m_height = m_imageContainer->m_height;
 
-            m_onloadHandlerRef.Call({});
+            if (!m_onloadHandlerRef.IsEmpty())
+            {
+                m_onloadHandlerRef.Call({});
+            }
         });
     }
 
@@ -116,8 +143,25 @@ namespace Babylon::Polyfills::Internal
         m_onloadHandlerRef = Napi::Persistent(eventHandler);
     }
 
+    void NativeCanvasImage::SetOnerror(const Napi::CallbackInfo&, const Napi::Value& value)
+    {
+        Napi::Function eventHandler{value.As<Napi::Function>()};
+        m_onerrorHandlerRef = Napi::Persistent(eventHandler);
+    }
+
     int NativeCanvasImage::CreateNVGImageForContext(NVGcontext* nvgContext) const
     {
         return nvgCreateImageRGBA(nvgContext, m_width, m_height, 0, static_cast<const unsigned char*>(m_imageContainer->m_data));
+    }
+
+    void NativeCanvasImage::HandleLoadImageError(const Napi::Error& error)
+    {
+        if (!m_onerrorHandlerRef.IsEmpty())
+        {
+            m_onerrorHandlerRef.Call({error.Value()});
+            return;
+        }
+
+        error.ThrowAsJavaScriptException();
     }
 }
