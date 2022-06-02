9e61a89ef299db7e627e296a56380541b957a9ac
Cedric Guillemet <1312968+CedricGuillemet@users.noreply.github.com>
2022-06-02 19:08:20 +0200

    Unity build (#1085)
    * unity build WIP
    
    * unity build working for Graphics and NativeEngine
    
    * more unity build projects
    
    * unity build with cmake
    
    * build fixes
    
    * no unity_build app runtime/apple
    
    * no unity xr/apple
    
    * no UB bgfx/apple
    
    * test without unity build
    
    * and back with UB
    
    * removed stb comment with implementation

--- Polyfills/Canvas/Source/Context.cpp
+++ Polyfills/Canvas/Source/Context.cpp
@@ -11,6 +11,7 @@
 
 #define STB_TRUETYPE_IMPLEMENTATION
 #include "stb/stb_truetype.h"
+#undef STB_TRUETYPE_IMPLEMENTATION
 #include "Canvas.h"
 #include "Context.h"
 #include "MeasureText.h"
@@ -23,7 +24,7 @@ Most of these context methods are preliminary work. They are currenbly not teste
 */
 namespace Babylon::Polyfills::Internal
 {
-    static constexpr auto JS_CONSTRUCTOR_NAME = "Context";
+    static constexpr auto JS_CONTEXT_CONSTRUCTOR_NAME = "Context";
 
     void Context::Initialize(Napi::Env env)
     {
@@ -31,7 +32,7 @@ namespace Babylon::Polyfills::Internal
 
         Napi::Function func = DefineClass(
             env,
-            JS_CONSTRUCTOR_NAME,
+            JS_CONTEXT_CONSTRUCTOR_NAME,
             {
                 InstanceMethod("clearRect", &Context::ClearRect),
                 InstanceMethod("save", &Context::Save),
@@ -74,14 +75,14 @@ namespace Babylon::Polyfills::Internal
                 InstanceAccessor("lineWidth", &Context::GetLineWidth, &Context::SetLineWidth),
                 InstanceAccessor("canvas", &Context::GetCanvas, nullptr)
             });
-        JsRuntime::NativeObject::GetFromJavaScript(env).Set(JS_CONSTRUCTOR_NAME, func);
+        JsRuntime::NativeObject::GetFromJavaScript(env).Set(JS_CONTEXT_CONSTRUCTOR_NAME, func);
     }
 
     Napi::Value Context::CreateInstance(Napi::Env env, NativeCanvas* canvas)
     {
         Napi::HandleScope scope{ env };
 
-        auto func = JsRuntime::NativeObject::GetFromJavaScript(env).Get(JS_CONSTRUCTOR_NAME).As<Napi::Function>();
+        auto func = JsRuntime::NativeObject::GetFromJavaScript(env).Get(JS_CONTEXT_CONSTRUCTOR_NAME).As<Napi::Function>();
         return func.New({ Napi::External<NativeCanvas>::New(env, canvas)});
     }
 
