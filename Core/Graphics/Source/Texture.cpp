e35f8cf3fec633a6575ca021acfc9afeb40c871c
Gary Hsu <bghgary@users.noreply.github.com>
2022-12-05 14:50:26 -0800

    Disown texture handle after attaching to frame buffer

--- Core/Graphics/Source/Texture.cpp
+++ Core/Graphics/Source/Texture.cpp
@@ -77,6 +77,11 @@ namespace Babylon::Graphics
         m_flags = flags;
     }
 
+    void Texture::Disown()
+    {
+        m_ownsHandle = false;
+    }
+
     bgfx::TextureHandle Texture::Handle() const
     {
         return m_handle;
