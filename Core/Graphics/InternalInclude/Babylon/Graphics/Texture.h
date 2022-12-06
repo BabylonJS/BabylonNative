3645a28f07b8e352d0680e4f37869fa151f23780
Gary Hsu <bghgary@users.noreply.github.com>
2022-12-05 17:35:52 -0800

    Render target texture improvements (#1173)

--- Core/Graphics/InternalInclude/Babylon/Graphics/Texture.h
+++ Core/Graphics/InternalInclude/Babylon/Graphics/Texture.h
@@ -24,6 +24,7 @@ namespace Babylon::Graphics
         void UpdateCube(uint16_t layer, uint8_t side, uint8_t mip, uint16_t x, uint16_t y, uint16_t width, uint16_t height, const bgfx::Memory* mem, uint16_t pitch = UINT16_MAX);
 
         void Attach(bgfx::TextureHandle handle, bool ownsHandle, uint16_t width, uint16_t height, bool hasMips, uint16_t numLayers, bgfx::TextureFormat::Enum format, uint64_t flags);
+        void Disown();
 
         bgfx::TextureHandle Handle() const;
         uint16_t Width() const;
