a9b85eb61e9a6c90312823151027acb18fbf6fe2
Sergio Ricardo Zerbetto Masson <sergioze@microsoft.com>
2023-03-27 13:48:26 -0300

    Removed possible broken test

--- Apps/Playground/Scripts/playground_runner.js
+++ Apps/Playground/Scripts/playground_runner.js
@@ -1,17 +1,7 @@
 if (typeof createScene === "function") {
     var engine = new BABYLON.NativeEngine({adaptToDeviceRatio: true});
     var scene = createScene();
-
-    if (scene.then) {
-        scene.then(scene => {
-            engine.runRenderLoop(function () {
-                scene.render();
-            });
-        });
-    }
-    else {
-        engine.runRenderLoop(function () {
-            scene.render();
-        });
-    }
+    engine.runRenderLoop(function () {
+        scene.render();
+    });
 }
