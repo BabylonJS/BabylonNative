6157904ce1cca5e686ba02bd27545534a1535ab2
Cedric Guillemet <1312968+CedricGuillemet@users.noreply.github.com>
2023-06-07 14:20:11 +0200

    up cmake extensions, removed unittests

--- Apps/UnitTests/Scripts/tests.js
+++ Apps/UnitTests/Scripts/tests.js
@@ -17,73 +17,7 @@ function BabylonReporter(runner) {
 
 mocha.setup({ ui: "bdd", reporter: BabylonReporter });
 
-function createRequest(method, url) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open(method, url);
-        xhr.addEventListener("loadend", () => resolve(xhr));
-        xhr.send();
-    });
-}
-
 const expect = chai.expect;
-
-describe("XMLHTTPRequest", function () {
-    this.timeout(0);
-    it("should have readyState=4 when load ends", async function () {
-        const xhr = await createRequest("GET", "https://httpbin.org/get");
-        expect(xhr.readyState).to.equal(4);
-    });
-    it("should have status=200 for a file that exists", async function () {
-        const xhr = await createRequest("GET", "https://httpbin.org/status/200");
-        expect(xhr.status).to.equal(200);
-    });
-    it("should load URLs with escaped unicode characters", async function () {
-        const xhr = await createRequest("GET", "https://raw.githubusercontent.com/BabylonJS/Assets/master/meshes/%CF%83%CF%84%CF%81%CE%BF%CE%B3%CE%B3%CF%85%CE%BB%CE%B5%CE%BC%CE%AD%CE%BD%CE%BF%CF%82%20%25%20%CE%BA%CF%8D%CE%B2%CE%BF%CF%82.glb");
-        expect(xhr.status).to.equal(200);
-    });
-    it("should load URLs with unescaped unicode characters", async function () {
-        const xhr = await createRequest("GET", "https://raw.githubusercontent.com/BabylonJS/Assets/master/meshes/στρογγυλεμένος%20%25%20κύβος.glb");
-        expect(xhr.status).to.equal(200);
-    });
-    it("should load URLs with unescaped unicode characters and spaces", async function () {
-        const xhr = await createRequest("GET", "https://raw.githubusercontent.com/BabylonJS/Assets/master/meshes/στρογγυλεμένος %25 κύβος.glb");
-        expect(xhr.status).to.equal(200);
-    });
-    it("should have status=404 for a file that does not exist", async function () {
-        const xhr = await createRequest("GET", "https://httpbin.org/status/404");
-        expect(xhr.status).to.equal(404);
-    });
-    it("should throw something when opening //", async function () {
-        function openDoubleSlash() {
-            const xhr = new XMLHttpRequest();
-            xhr.open("GET", "//");
-            xhr.send();
-        }
-        expect(openDoubleSlash).to.throw();
-    });
-    it("should throw something when opening a url with no scheme", function () {
-        function openNoProtocol() {
-            const xhr = new XMLHttpRequest();
-            xhr.open("GET", "noscheme.glb");
-            xhr.send();
-        }
-        expect(openNoProtocol).to.throw();
-    });
-    it("should throw something when sending before opening", function () {
-        function sendWithoutOpening() {
-            const xhr = new XMLHttpRequest();
-            xhr.send();
-        }
-        expect(sendWithoutOpening).to.throw();
-    });
-    it("should open a local file", async function () {
-        const xhr = await createRequest("GET", "app:///Scripts/tests.js");
-        expect(xhr).to.have.property('readyState', 4);
-        expect(xhr).to.have.property('status', 200);
-        expect(xhr).to.have.property('responseText').with.lengthOf.above(0);
-    });
-});
 
 describe("RequestFile", function () {
     this.timeout(0);
