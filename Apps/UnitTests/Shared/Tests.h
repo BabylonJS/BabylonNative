3e00929c1272a3d11e9ae3e12742f555abbeeee9
Cedric Guillemet <1312968+CedricGuillemet@users.noreply.github.com>
2023-07-05 17:34:04 +0200

    Google Tests Experiment

--- Apps/UnitTests/Shared/Tests.h
+++ Apps/UnitTests/Shared/Tests.h
@@ -1,3 +1,4 @@
+#include "gtest/gtest.h"
 #include <UrlLib/UrlLib.h>
 #include <arcana/threading/task.h>
 #include <future>
@@ -15,6 +16,8 @@
 
 std::promise<int32_t> exitCode;
 
+Babylon::Graphics::Configuration deviceTestConfig{};
+
 static inline constexpr const char* JS_FUNCTION_NAME{ "SetExitCode" };
 void SetExitCode(const Napi::CallbackInfo& info)
 {
@@ -26,30 +29,30 @@ void SetExitCode(const Napi::CallbackInfo& info)
     exitCode.set_value(info[0].As<Napi::Number>().Int32Value());
 }
 
-int Run(Babylon::Graphics::Device device)
+TEST(JSTest, Test0)
 {
+    Babylon::Graphics::Device device = deviceTestConfig;
+
     std::optional<Babylon::Polyfills::Canvas> nativeCanvas;
 
     Babylon::AppRuntime runtime{};
-    runtime.Dispatch([&device, &nativeCanvas](Napi::Env env)
-    {
+    runtime.Dispatch([&device, &nativeCanvas](Napi::Env env) {
         device.AddToJavaScript(env);
 
         Babylon::Polyfills::XMLHttpRequest::Initialize(env);
-        Babylon::Polyfills::Console::Initialize(env, [](const char* message, auto)
-        {
+        Babylon::Polyfills::Console::Initialize(env, [](const char* message, auto) {
             printf("%s", message);
             fflush(stdout);
         });
         Babylon::Polyfills::Window::Initialize(env);
         nativeCanvas.emplace(Babylon::Polyfills::Canvas::Initialize(env));
         Babylon::Plugins::NativeEngine::Initialize(env);
-        
+
         env.Global().Set(JS_FUNCTION_NAME, Napi::Function::New(env, SetExitCode, JS_FUNCTION_NAME));
     });
 
     Babylon::ScriptLoader loader{runtime};
-    loader.Eval("global = {};", ""); // Required for Chai.js as we do not have global in Babylon Native
+    loader.Eval("global = {};", "");             // Required for Chai.js as we do not have global in Babylon Native
     loader.Eval("location = { href: '' };", ""); // Required for Mocha.js as we do not have a location in Babylon Native
     loader.LoadScript("app:///Scripts/babylon.max.js");
     loader.LoadScript("app:///Scripts/babylonjs.materials.js");
@@ -61,5 +64,11 @@ int Run(Babylon::Graphics::Device device)
     device.FinishRenderingCurrentFrame();
 
     auto code{exitCode.get_future().get()};
-    return code;
+    EXPECT_EQ(code, 0);
+}
+
+int Run()
+{
+    testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
 }
