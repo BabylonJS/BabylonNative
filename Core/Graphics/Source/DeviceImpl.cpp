2dd675f5b9a3b2592251404369a49b10cf42ea1f
Cedric Guillemet <1312968+CedricGuillemet@users.noreply.github.com>
2023-12-20 17:44:00 +0100

    flipping happens after rendering (#1336)
    bgfx default is to do page flip before rendering a new frame. So,
    rendering 1 frame is not enough to see rendering on screen.
    This 1 frame latency was visible on Apple and fixed with this PR:
    https://github.com/BabylonJS/BabylonNative/pull/525
    Unfortunately, the issue has been reported on Windows as well. With this
    PR, flipping will always be after rendering.
    Related posts: 
    https://github.com/bkaradzic/bgfx/issues/339

--- Core/Graphics/Source/DeviceImpl.cpp
+++ Core/Graphics/Source/DeviceImpl.cpp
@@ -32,12 +32,8 @@ namespace Babylon::Graphics
 
         auto& init = m_state.Bgfx.InitState;
         init.type = s_bgfxRenderType;
-        init.resolution.reset = BGFX_RESET_VSYNC | BGFX_RESET_MAXANISOTROPY;
+        init.resolution.reset = BGFX_RESET_VSYNC | BGFX_RESET_MAXANISOTROPY | BGFX_RESET_FLIP_AFTER_RENDER;
         init.resolution.maxFrameLatency = 1;
-        if (s_bgfxFlipAfterRender)
-        {
-            init.resolution.reset |= BGFX_RESET_FLIP_AFTER_RENDER;
-        }
 
         init.callback = &m_bgfxCallback;
 
