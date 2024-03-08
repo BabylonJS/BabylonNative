fb2086a07233684360a4ac87d108d4bded79296b
Gary Hsu <bghgary@users.noreply.github.com>
2024-03-08 09:58:44 -0800

    Support async playgrounds

--- Apps/Playground/Scripts/playground_runner.js
+++ Apps/Playground/Scripts/playground_runner.js
@@ -1,7 +1,15 @@
 if (typeof createScene === "function") {
     var engine = new BABYLON.NativeEngine({adaptToDeviceRatio: true});
     var scene = createScene();
-    engine.runRenderLoop(function () {
-        scene.render();
-    });
+    if (scene.then) {
+        scene.then(function (scene) {
+            engine.runRenderLoop(function () {
+                scene.render();
+            });
+        })
+    } else {
+        engine.runRenderLoop(function () {
+            scene.render();
+        });
+    }
 }
