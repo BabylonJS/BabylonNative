6db96c45753bbc6581f6c63ac8dc25c78b120133
Gary Hsu <bghgary@users.noreply.github.com>
2024-03-21 15:51:57 -0700

    Bring in latest JsRuntimeHost with V8 inspector fixes Also includes various testing fixes

--- Apps/UnitTests/Scripts/tests.js
+++ Apps/UnitTests/Scripts/tests.js
@@ -354,9 +354,9 @@ mocha.run(failures => {
     // Test program will wait for code to be set before exiting
     if (failures > 0) {
         // Failure
-        SetExitCode(1);
+        setExitCode(1);
     } else {
         // Success
-        SetExitCode(0);
+        setExitCode(0);
     }
 });
