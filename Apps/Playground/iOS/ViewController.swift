BabylonJS/BabylonNative

commit 39ce2bca1f7cdaa43e5e9458c8f186059100b774

fix: cleanup view controller (#1368)
This PR cleans up view controller in iOS playground from explicit
unwraps. It should give a better example of how people can use
BabylonNative in their projects

Apps/Playground/iOS/ViewController.swift
@@ -1,8 +1,6 @@
 import UIKit
 import MetalKit
-
 import UIKit
-import UIKit.UIGestureRecognizerSubclass
 
 /**
   * A very simple gesture recognizer.  All that it does is to emulate the functionality found in other platforms
@@ -29,14 +27,11 @@ class UIBabylonGestureRecognizer: UIGestureRecognizer {
         super.touchesBegan(touches, with: event)
         
         for touch in touches {
-            let deviceSlot = _activeTouchIds.firstIndex(of: -1)
+            guard let deviceSlot = _activeTouchIds.firstIndex(of: -1) else { continue }
+            _activeTouchIds[deviceSlot] = touch.hash
+            let loc = touch.location(in: view)
             
-            if (deviceSlot != nil) {
-                _activeTouchIds[deviceSlot!] = touch.hash
-                let loc = touch.location(in: view)
-                
-                _onTouchDown(Int32(deviceSlot!), Int32(loc.x), Int32(loc.y))
-            }
+            _onTouchDown(Int32(deviceSlot), Int32(loc.x), Int32(loc.y))
         }
     }
     
@@ -44,12 +39,10 @@ class UIBabylonGestureRecognizer: UIGestureRecognizer {
         super.touchesMoved(touches, with: event)
         
         for touch in touches {
-            let deviceSlot = _activeTouchIds.firstIndex(of: touch.hash)
-            if (deviceSlot != nil) {
-                let loc = touch.location(in: view)
-                
-                _onTouchMove(Int32(deviceSlot!), Int32(loc.x), Int32(loc.y))
-            }
+            guard let deviceSlot = _activeTouchIds.firstIndex(of: touch.hash) else { continue }
+            let loc = touch.location(in: view)
+            
+            _onTouchMove(Int32(deviceSlot), Int32(loc.x), Int32(loc.y))
         }
     }
     
@@ -57,18 +50,16 @@ class UIBabylonGestureRecognizer: UIGestureRecognizer {
         super.touchesEnded(touches, with: event)
         
         for touch in touches {
-            let deviceSlot = _activeTouchIds.firstIndex(of: touch.hash)
-            if (deviceSlot != nil) {
-                let loc = touch.location(in: view)
-                
-                _onTouchUp(Int32(deviceSlot!), Int32(loc.x), Int32(loc.y))
-                _activeTouchIds[deviceSlot!] = -1
-            }
+            guard let deviceSlot = _activeTouchIds.firstIndex(of: touch.hash) else { continue }
+            let loc = touch.location(in: view)
+            
+            _onTouchUp(Int32(deviceSlot), Int32(loc.x), Int32(loc.y))
+            _activeTouchIds[deviceSlot] = -1
         }
     }
 }
 
-class ViewController: UIViewController, MTKViewDelegate {
+class ViewController: UIViewController {
 
     var mtkView: MTKView!
     var xrView: MTKView!
@@ -79,54 +70,70 @@ class ViewController: UIViewController, MTKViewDelegate {
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        let appDelegate = UIApplication.shared.delegate as? AppDelegate
-        if appDelegate != nil {
-            mtkView = MTKView()
-            mtkView.delegate = self
-            mtkView.translatesAutoresizingMaskIntoConstraints = false
-            view.addSubview(mtkView)
-            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "|[mtkView]|", options: [], metrics: nil, views: ["mtkView" : mtkView]))
-            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[mtkView]|", options: [], metrics: nil, views: ["mtkView" : mtkView]))
-
-            let device = MTLCreateSystemDefaultDevice()!
-            mtkView.device = device
-
-            mtkView.colorPixelFormat = .bgra8Unorm_srgb
-            mtkView.depthStencilPixelFormat = .depth32Float
-
-            // Simple gesture recognizer, just provides platform to handle input events
-            let gesture = UIBabylonGestureRecognizer(target: self, onTouchDown: appDelegate!._bridge!.setTouchDown, onTouchMove: appDelegate!._bridge!.setTouchMove, onTouchUp: appDelegate!._bridge!.setTouchUp)
-            mtkView.addGestureRecognizer(gesture)
-
-            let scale = view.contentScaleFactor
-            let width = view.bounds.size.width
-            let height = view.bounds.size.height
-
-            xrView = MTKView()
-            xrView.translatesAutoresizingMaskIntoConstraints = false
-            xrView.isUserInteractionEnabled = false
-            xrView.isHidden = true
-            view.addSubview(xrView)
-            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "|[xrView]|", options: [], metrics: nil, views: ["xrView" : xrView]))
-            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[xrView]|", options: [], metrics: nil, views: ["xrView" : xrView]))
-
-            appDelegate!._bridge!.init(mtkView, screenScale:Float(UIScreen.main.scale), width:Int32(width * scale), height:Int32(height * scale), xrView:Unmanaged.passUnretained(xrView).toOpaque())
-        }
+        guard
+            let appDelegate = UIApplication.shared.delegate as? AppDelegate,
+            let bridge = appDelegate._bridge
+        else { return }
+        
+        setupViews()
+        
+        let device = MTLCreateSystemDefaultDevice()
+        mtkView.device = device
+        
+        mtkView.colorPixelFormat = .bgra8Unorm_srgb
+        mtkView.depthStencilPixelFormat = .depth32Float
+        
+        // Simple gesture recognizer, just provides platform to handle input events
+        let gesture = UIBabylonGestureRecognizer(
+            target: self,
+            onTouchDown: bridge.setTouchDown,
+            onTouchMove: bridge.setTouchMove,
+            onTouchUp: bridge.setTouchUp
+        )
+        mtkView.addGestureRecognizer(gesture)
+        
+        let scale = view.contentScaleFactor
+        let width = view.bounds.size.width
+        let height = view.bounds.size.height
+        
+        bridge.init(
+            mtkView,
+            screenScale:Float(UIScreen.main.scale),
+            width:Int32(width * scale),
+            height:Int32(height * scale),
+            xrView:Unmanaged.passUnretained(xrView).toOpaque()
+        )
     }
+  
+    func setupViews() {
+        mtkView = MTKView()
+        mtkView.delegate = self
+        mtkView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(mtkView)
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "|[mtkView]|", options: [], metrics: nil, views: ["mtkView" : mtkView]))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[mtkView]|", options: [], metrics: nil, views: ["mtkView" : mtkView]))
+        
+        xrView = MTKView()
+        xrView.translatesAutoresizingMaskIntoConstraints = false
+        xrView.isUserInteractionEnabled = false
+        xrView.isHidden = true
+        view.addSubview(xrView)
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "|[xrView]|", options: [], metrics: nil, views: ["xrView" : xrView]))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[xrView]|", options: [], metrics: nil, views: ["xrView" : xrView]))
+    }
+}
 
+// MARK: MTKViewDelegate
+extension ViewController: MTKViewDelegate {
     func draw(in view: MTKView) {
-        let appDelegate = UIApplication.shared.delegate as? AppDelegate
-        if appDelegate != nil {
-            xrView.isHidden = !appDelegate!._bridge!.isXRActive()
-            appDelegate!._bridge!.render()
-        }
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        xrView.isHidden = appDelegate._bridge?.isXRActive() ?? false
+        appDelegate._bridge?.render()
     }
-
+    
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
-        let appDelegate = UIApplication.shared.delegate as? AppDelegate
-        if appDelegate != nil {
-            appDelegate!._bridge!.resize(Int32(size.width), height: Int32(size.height))
-        }
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        appDelegate._bridge?.resize(Int32(size.width), height: Int32(size.height))
     }
 }
 

commit 737bc8e6659016caa99ae051103674494d68bd73

fix xrView visibility iOS (#1380)
Fix bug introduced with
https://github.com/BabylonJS/BabylonNative/pull/1368
xrView.isHidden was not the intended value.
I've kept the syntax proposed by @okwasniewski
idk if it's the 'correct' and standard way to do it.
cc @Allan121

fixes #1379

Apps/Playground/iOS/ViewController.swift
@@ -126,7 +126,7 @@ class ViewController: UIViewController {
 extension ViewController: MTKViewDelegate {
     func draw(in view: MTKView) {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
-        xrView.isHidden = appDelegate._bridge?.isXRActive() ?? false
+        xrView.isHidden = !(appDelegate._bridge?.isXRActive() ?? false)
         appDelegate._bridge?.render()
     }
     