a818f07ad19e50db296ce22c26728048efc11af6
Cedric Guillemet <1312968+CedricGuillemet@users.noreply.github.com>
2024-06-13 19:21:46 +0200

    sleep_for because unable to expect a crash with gtest

--- Apps/UnitTests/Shared/Shared.cpp
+++ Apps/UnitTests/Shared/Shared.cpp
@@ -98,81 +98,43 @@ TEST(JavaScript, All)
     EXPECT_EQ(exitCode, 0);
 }
 
-/*
-This test does a serie of initialization and shutdowns.
-It needs the shutdown PR to be merged before running properly.
-TEST(NativeAPI, LifeCycle)
-{
-    for (int cycle = 0; cycle < 20; cycle++)
-    {
-        Babylon::Graphics::Device device{deviceConfig};
-        std::optional<Babylon::Polyfills::Canvas> nativeCanvas;
-
-        Babylon::AppRuntime runtime{};
-        runtime.Dispatch([&device, &nativeCanvas](Napi::Env env) {
-            device.AddToJavaScript(env);
-
-            Babylon::Polyfills::XMLHttpRequest::Initialize(env);
-            Babylon::Polyfills::Console::Initialize(env, [](const char* message, auto) {
-                printf("%s", message);
-                fflush(stdout);
-            });
-            Babylon::Polyfills::Window::Initialize(env);
-            nativeCanvas.emplace(Babylon::Polyfills::Canvas::Initialize(env));
-            Babylon::Plugins::NativeEngine::Initialize(env);
-        });
-
-        Babylon::ScriptLoader loader{runtime};
-        loader.LoadScript("app:///Scripts/babylon.max.js");
-        loader.LoadScript("app:///Scripts/babylonjs.materials.js");
-
-        for (int frame = 0; frame < 10; frame++)
-        {
-            device.StartRenderingCurrentFrame();
-            device.FinishRenderingCurrentFrame();
-        }
-    }
-}
-*/
 TEST(Shutdown, AsyncShaderCompilation)
 {
     std::promise<int32_t> exitCodePromise;
 
-    //for (int cycle = 0; cycle < 20; cycle++)
-    {
-        Babylon::Graphics::Device device{deviceConfig};
-        auto update{ device.GetUpdate("update") };
-        std::optional<Babylon::Polyfills::Canvas> nativeCanvas;
+    Babylon::Graphics::Device device{ deviceConfig };
+    auto update{ device.GetUpdate("update") };
+    std::optional<Babylon::Polyfills::Canvas> nativeCanvas;
 
-        Babylon::AppRuntime runtime{};
-        runtime.Dispatch([&device, &nativeCanvas, &exitCodePromise](Napi::Env env) {
-            device.AddToJavaScript(env);
+    Babylon::AppRuntime runtime{};
+    runtime.Dispatch([&device, &nativeCanvas, &exitCodePromise](Napi::Env env) {
+        device.AddToJavaScript(env);
 
-            Babylon::Polyfills::XMLHttpRequest::Initialize(env);
-            Babylon::Polyfills::Console::Initialize(env, [](const char* message, auto) {
-                printf("%s", message);
-                fflush(stdout);
+        Babylon::Polyfills::XMLHttpRequest::Initialize(env);
+        Babylon::Polyfills::Console::Initialize(env, [](const char* message, auto) {
+            std::cout << message << std::endl;
+            std::cout.flush();
             });
-            Babylon::Polyfills::Window::Initialize(env);
+        Babylon::Polyfills::Window::Initialize(env);
 
-            Babylon::Polyfills::XMLHttpRequest::Initialize(env);
+        Babylon::Polyfills::XMLHttpRequest::Initialize(env);
 
-            nativeCanvas.emplace(Babylon::Polyfills::Canvas::Initialize(env));
+        nativeCanvas.emplace(Babylon::Polyfills::Canvas::Initialize(env));
 
-            Babylon::Plugins::NativeEngine::Initialize(env);
+        Babylon::Plugins::NativeEngine::Initialize(env);
 
-            auto setExitCodeCallback = Napi::Function::New(
-                env, [&exitCodePromise](const Napi::CallbackInfo& info) {
-                    Napi::Env env = info.Env();
-                    exitCodePromise.set_value(info[0].As<Napi::Number>().Int32Value());
-                }, "setExitCode");
-            env.Global().Set("setExitCode", setExitCodeCallback);
+        auto setExitCodeCallback = Napi::Function::New(
+            env, [&exitCodePromise](const Napi::CallbackInfo& info) {
+                Napi::Env env = info.Env();
+                exitCodePromise.set_value(info[0].As<Napi::Number>().Int32Value());
+            }, "setExitCode");
+        env.Global().Set("setExitCode", setExitCodeCallback);
         });
 
-        Babylon::ScriptLoader loader{runtime};
-        loader.LoadScript("app:///Scripts/babylon.max.js");
-        loader.LoadScript("app:///Scripts/babylonjs.materials.js");
-        loader.Eval(R"(
+    Babylon::ScriptLoader loader{ runtime };
+    loader.LoadScript("app:///Scripts/babylon.max.js");
+    loader.LoadScript("app:///Scripts/babylonjs.materials.js");
+    loader.Eval(R"(
             function CreateBoxAsync(scene) {
                 BABYLON.Mesh.CreateBox("box1", 0.2, scene);
                 return Promise.resolve();
@@ -199,13 +161,16 @@ TEST(Shutdown, AsyncShaderCompilation)
             });
             )", "script");
 
-        auto exitCode{ exitCodePromise.get_future().get() };
-        device.StartRenderingCurrentFrame();
-        update.Start();
+    auto exitCode{ exitCodePromise.get_future().get() };
+    device.StartRenderingCurrentFrame();
+    update.Start();
 
-        update.Finish();
-        device.FinishRenderingCurrentFrame();
-    }
+    update.Finish();
+    device.FinishRenderingCurrentFrame();
+
+    // this sleep makes sure shader is finished loading/compiling before exit.
+    // Once shutdown is fixed, remove this sleep call.
+    std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
 TEST(Performance, Spheres)
