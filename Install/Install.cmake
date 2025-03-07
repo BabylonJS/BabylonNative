BabylonJS/BabylonNative

commit 5f4ede90702f227a442d6ff6b764d76183fd4082

Nanovg shaders in canvas (#1472)
- needed for uwp : ~~https://github.com/bkaradzic/bimg/pull/92~~
(merged)
- update bimg, bx, bgfx
- upgrade to VS2022 for pragma intrinsics in bx
- replace bgfx.cmake fork with Branimir's fork. Files copy pasted into
babylon fork.
- Canvas will build nanovg shaders with shaderc (also built)
- check target glslang in dependencies to not have 2 targets with same
name (collision with bgfx 3rd party)
- target names changed (bimg_encode/decode vs nvtt ...)
- use static_assert instead of bx version
- added `libwayland-dev` Ubuntu build dep

Known limitation:
There is a discrepancy with glslang output library. **You can generate
shaders by building Canvas library but Shader tool needs to be disabled
to build UnitTest and Playground**. Best solution is to update glslang
dep. bgfx 3rd party glslang is more recent but doesn't contain
modification from our forks.

Install/Install.cmake
@@ -35,7 +35,7 @@ endfunction()
 install_targets(arcana)
 
 ## bgfx
-install_targets(astc-encoder edtaa3 etc1 etc2 iqa nvtt pvrtc squish tinyexr bgfx bimg bx)
+install_targets(bimg_encode bimg_decode bgfx bimg bx)
 
 ## glslang
 install_targets(GenericCodeGen glslang MachineIndependent OGLCompiler OSDependent SPIRV glslang-default-resource-limits)

Install/Test/CMakeLists.txt
@@ -141,20 +141,17 @@ target_link_directories(TestInstall PRIVATE ${INSTALL_LIBS_DIR} ${V8JSI_LIB_PATH
 target_link_libraries(TestInstall
     AppRuntime
     arcana
-    astc-encoder
+    bimg_encode
+    bimg_decode
     bgfx
     bimg
     bx
     Canvas
     Console
-    edtaa3
-    etc1
-    etc2
     ExternalTexture
     GenericCodeGen
     glslang
     Graphics
-    iqa
     JsRuntime
     MachineIndependent
     napi
@@ -165,17 +162,13 @@ target_link_libraries(TestInstall
     NativeOptimizations
     NativeTracing
     NativeXr
-    nvtt
     OGLCompiler
     openxr_loader
     OSDependent
-    pvrtc
     ScriptLoader
     spirv-cross-core
     spirv-cross-hlsl
     SPIRV
-    squish
-    tinyexr
     UrlLib
     Window
     XMLHttpRequest

CMakeLists.txt
@@ -23,7 +23,7 @@ FetchContent_Declare(base-n
     GIT_TAG 7573e77c0b9b0e8a5fb63d96dbde212c921993b4)
 FetchContent_Declare(bgfx.cmake
     GIT_REPOSITORY https://github.com/BabylonJS/bgfx.cmake.git
-    GIT_TAG ecaffce0d2f7170c2f060a606908dc6a9163669f)
+    GIT_TAG fdf3ed4a62acabd7025d0a843f6d8a9aff5e3612)
 FetchContent_Declare(CMakeExtensions
     GIT_REPOSITORY https://github.com/BabylonJS/CMakeExtensions.git
     GIT_TAG ea28b7689530bfdc4905806f27ecf7e8ed4b5419)
@@ -182,6 +182,7 @@ if(BABYLON_NATIVE_BUILD_APPS)
     add_subdirectory(Apps)
 endif()
 
-if(BABYLON_NATIVE_INSTALL)
+# no install for glslang compiled for shaderc
+if(BABYLON_NATIVE_INSTALL AND NOT BGFX_BUILD_TOOLS)
     include(Install/Install.cmake)
 endif()

Core/Graphics/CMakeLists.txt
@@ -44,6 +44,8 @@ target_link_libraries(Graphics
     PRIVATE JsRuntimeInternal
     PRIVATE bgfx
     PRIVATE bimg
+    PRIVATE bimg_encode
+    PRIVATE bimg_decode
     PRIVATE bx)
 
 target_compile_definitions(Graphics

Dependencies/CMakeLists.txt
@@ -52,24 +52,18 @@ elseif(GRAPHICS_API STREQUAL "OpenGL")
 elseif(GRAPHICS_API STREQUAL "Vulkan")
     target_compile_definitions(bgfx PRIVATE BGFX_CONFIG_RENDERER_VULKAN=1)
 endif()
-set_property(TARGET astc-encoder PROPERTY FOLDER Dependencies/bgfx/3rdparty)
-set_property(TARGET astc-encoder PROPERTY UNITY_BUILD false)
-set_property(TARGET edtaa3 PROPERTY FOLDER Dependencies/bgfx/3rdparty)
-set_property(TARGET etc1 PROPERTY FOLDER Dependencies/bgfx/3rdparty)
-set_property(TARGET etc2 PROPERTY FOLDER Dependencies/bgfx/3rdparty)
-set_property(TARGET iqa PROPERTY FOLDER Dependencies/bgfx/3rdparty)
-set_property(TARGET nvtt PROPERTY FOLDER Dependencies/bgfx/3rdparty)
-set_property(TARGET nvtt PROPERTY UNITY_BUILD false)
-set_property(TARGET pvrtc PROPERTY FOLDER Dependencies/bgfx/3rdparty)
-set_property(TARGET squish PROPERTY FOLDER Dependencies/bgfx/3rdparty)
-set_property(TARGET squish PROPERTY UNITY_BUILD false)
+set_property(TARGET bimg PROPERTY FOLDER Dependencies/bgfx/3rdparty)
+set_property(TARGET bimg PROPERTY UNITY_BUILD false)
+set_property(TARGET bimg_encode PROPERTY FOLDER Dependencies/bgfx/3rdparty)
+set_property(TARGET bimg_encode PROPERTY UNITY_BUILD false)
+set_property(TARGET bimg_decode PROPERTY FOLDER Dependencies/bgfx/3rdparty)
+set_property(TARGET bimg_decode PROPERTY UNITY_BUILD false)
 if(TARGET tinyexr)
     set_property(TARGET tinyexr PROPERTY FOLDER Dependencies/bgfx/3rdparty)
 endif()
 set_property(TARGET bgfx PROPERTY FOLDER Dependencies/bgfx)
 set_property(TARGET bimg PROPERTY FOLDER Dependencies/bgfx)
 set_property(TARGET bx PROPERTY FOLDER Dependencies/bgfx)
-
 target_compile_definitions(bx PRIVATE _CRT_SECURE_NO_WARNINGS)
 
 if(APPLE)
@@ -93,15 +87,18 @@ set(ENABLE_CTEST OFF CACHE BOOL "")
 set(SKIP_GLSLANG_INSTALL ON CACHE BOOL "")
 set(ENABLE_GLSLANG_WEBMIN ON CACHE BOOL "")
 set(ENABLE_GLSLANG_WEBMIN_DEVEL OFF CACHE BOOL "")
-FetchContent_MakeAvailable_With_Message(glslang)
 
-set_property(TARGET GenericCodeGen PROPERTY FOLDER Dependencies/glslang)
-set_property(TARGET glslang PROPERTY FOLDER Dependencies/glslang)
-set_property(TARGET glslang-default-resource-limits PROPERTY FOLDER Dependencies/glslang)
-set_property(TARGET OGLCompiler PROPERTY FOLDER Dependencies/glslang)
-set_property(TARGET OSDependent PROPERTY FOLDER Dependencies/glslang)
-set_property(TARGET MachineIndependent PROPERTY FOLDER Dependencies/glslang)
-set_property(TARGET SPIRV PROPERTY FOLDER Dependencies/glslang)
+if(NOT TARGET glslang)
+    FetchContent_MakeAvailable_With_Message(glslang)
+
+    set_property(TARGET GenericCodeGen PROPERTY FOLDER Dependencies/glslang)
+    set_property(TARGET glslang PROPERTY FOLDER Dependencies/glslang)
+    set_property(TARGET glslang-default-resource-limits PROPERTY FOLDER Dependencies/glslang)
+    set_property(TARGET OGLCompiler PROPERTY FOLDER Dependencies/glslang)
+    set_property(TARGET OSDependent PROPERTY FOLDER Dependencies/glslang)
+    set_property(TARGET MachineIndependent PROPERTY FOLDER Dependencies/glslang)
+    set_property(TARGET SPIRV PROPERTY FOLDER Dependencies/glslang)
+endif()
 
 # --------------------------------------------------
 # googletest
@@ -180,8 +177,12 @@ foreach(target spirv-cross-core spirv-cross-glsl spirv-cross-msl spirv-cross-hls
 endforeach()
 
 # seen with Sergio, disable warnings until unused parameter [-Wunused-parameter] get fixed. 
-disable_warnings(SPIRV)
-disable_warnings(MachineIndependent)
+if(TARGET SPIRV)
+    disable_warnings(SPIRV)
+endif()
+if(TARGET MachineIndependent)
+    disable_warnings(MachineIndependent)
+endif()
 
 # --------------------------------------------------
 # xr

Plugins/NativeEngine/CMakeLists.txt
@@ -40,6 +40,8 @@ target_link_libraries(NativeEngine
     PRIVATE arcana
     PRIVATE bgfx
     PRIVATE bimg
+    PRIVATE bimg_encode
+    PRIVATE bimg_decode
     PRIVATE bx
     PRIVATE glslang
     PRIVATE glslang-default-resource-limits

Polyfills/Canvas/CMakeLists.txt
@@ -1,3 +1,93 @@
+
+function(add_bgfx_shader FILE FOLDER)
+    get_filename_component(FILENAME "${FILE}" NAME_WE)
+    string(SUBSTRING "${FILENAME}" 0 2 TYPE)
+    if("${TYPE}" STREQUAL "fs")
+        set(TYPE "FRAGMENT")
+    elseif("${TYPE}" STREQUAL "vs")
+        set(TYPE "VERTEX")
+    elseif("${TYPE}" STREQUAL "cs")
+        set(TYPE "COMPUTE")
+    else()
+        set(TYPE "")
+    endif()
+
+    if(NOT "${TYPE}" STREQUAL "")
+        set(COMMON FILE ${FILE} ${TYPE} INCLUDES ${CMAKE_CURRENT_SOURCE_DIR}/Source/Shaders ${BGFX_DIR}/src)
+        set(OUTPUTS "")
+        set(OUTPUTS_PRETTY "")
+
+        # dx11
+        set(DX11_OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/Source/Shaders/dx11/${FILENAME}.h)
+        if(NOT "${TYPE}" STREQUAL "COMPUTE")
+            _bgfx_shaderc_parse(
+                DX11 ${COMMON} WINDOWS
+                PROFILE s_5_0
+                O 3
+                OUTPUT ${DX11_OUTPUT}
+                BIN2C "${FILENAME}_dx11"
+            )
+        else()
+            _bgfx_shaderc_parse(
+                DX11 ${COMMON} WINDOWS
+                PROFILE s_5_0
+                O 1
+                OUTPUT ${DX11_OUTPUT}
+                BIN2C "${FILENAME}_dx11"
+            )
+        endif()
+        list(APPEND OUTPUTS "DX11")
+        set(OUTPUTS_PRETTY "${OUTPUTS_PRETTY}DX11, ")
+
+        # metal
+        set(METAL_OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/Source/Shaders/metal/${FILENAME}.h)
+        _bgfx_shaderc_parse(METAL ${COMMON} OSX PROFILE metal OUTPUT ${METAL_OUTPUT} BIN2C "${FILENAME}_mtl")
+        list(APPEND OUTPUTS "METAL")
+        set(OUTPUTS_PRETTY "${OUTPUTS_PRETTY}Metal, ")
+
+        # essl
+        if(NOT "${TYPE}" STREQUAL "COMPUTE")
+            set(ESSL_OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/Source/Shaders/essl/${FILENAME}.h)
+            _bgfx_shaderc_parse(ESSL ${COMMON} ANDROID PROFILE 100_es OUTPUT ${ESSL_OUTPUT} BIN2C "${FILENAME}_essl")
+            list(APPEND OUTPUTS "ESSL")
+            set(OUTPUTS_PRETTY "${OUTPUTS_PRETTY}ESSL, ")
+        endif()
+
+        # glsl
+        set(GLSL_OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/Source/Shaders/glsl/${FILENAME}.h)
+        if(NOT "${TYPE}" STREQUAL "COMPUTE")
+            _bgfx_shaderc_parse(GLSL ${COMMON} LINUX PROFILE 140 OUTPUT ${GLSL_OUTPUT} BIN2C "${FILENAME}_glsl")
+        else()
+            _bgfx_shaderc_parse(GLSL ${COMMON} LINUX PROFILE 430 OUTPUT ${GLSL_OUTPUT} BIN2C "${FILENAME}_glsl")
+        endif()
+        list(APPEND OUTPUTS "GLSL")
+        set(OUTPUTS_PRETTY "${OUTPUTS_PRETTY}GLSL, ")
+
+        # spirv
+        if(NOT "${TYPE}" STREQUAL "COMPUTE")
+            set(SPIRV_OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/Source/Shaders/spirv/${FILENAME}.h)
+            _bgfx_shaderc_parse(SPIRV ${COMMON} LINUX PROFILE spirv OUTPUT ${SPIRV_OUTPUT} BIN2C "${FILENAME}_spv")
+            list(APPEND OUTPUTS "SPIRV")
+            set(OUTPUTS_PRETTY "${OUTPUTS_PRETTY}SPIRV")
+            set(OUTPUT_FILES "")
+            set(COMMANDS "")
+        endif()
+
+        foreach(OUT ${OUTPUTS})
+            list(APPEND OUTPUT_FILES ${${OUT}_OUTPUT})
+            list(APPEND COMMANDS COMMAND "bgfx::shaderc" ${${OUT}})
+            get_filename_component(OUT_DIR ${${OUT}_OUTPUT} DIRECTORY)
+            file(MAKE_DIRECTORY ${OUT_DIR})
+        endforeach()
+
+        file(RELATIVE_PATH PRINT_NAME ${CMAKE_CURRENT_SOURCE_DIR}/Source/Shaders ${FILE})
+        add_custom_command(
+            MAIN_DEPENDENCY ${FILE} OUTPUT ${OUTPUT_FILES} ${COMMANDS}
+            COMMENT "Compiling shader ${PRINT_NAME} for ${OUTPUTS_PRETTY}"
+        )
+    endif()
+endfunction()
+
 set(SOURCES
     "Include/Babylon/Polyfills/Canvas.h"
     "Source/Canvas.cpp"
@@ -15,14 +105,17 @@ set(SOURCES
     "Source/nanovg_babylon.h"
     )
 
+file(GLOB SHADERS "Source/Shaders/*.sc" "Source/Shaders/*.sh")
+
 file(GLOB FONT_SOURCES ${BGFX_DIR}/examples/common/font/*.cpp)
 file(GLOB NANOVG_SOURCES ${BGFX_DIR}/examples/common/nanovg/nanovg.cpp)
 set(ATLAS_SOURCES ${BGFX_DIR}/examples/common/cube_atlas.cpp)
 
-add_library(Canvas ${SOURCES} ${FONT_SOURCES} ${ATLAS_SOURCES} ${NANOVG_SOURCES})
+add_library(Canvas ${SOURCES} ${FONT_SOURCES} ${ATLAS_SOURCES} ${NANOVG_SOURCES} ${SHADERS})
 
 target_include_directories(Canvas
     PUBLIC "Include"
+    PRIVATE "Source"
     PRIVATE "${BGFX_DIR}/3rdparty"
     PRIVATE "${BGFX_DIR}/examples/common"
     PRIVATE "${BGFX_DIR}/examples/common/nanovg")
@@ -37,7 +130,17 @@ target_link_libraries(Canvas
     PRIVATE UrlLib
     PRIVATE base-n)
 
+if(BGFX_BUILD_TOOLS AND BGFX_BUILD_TOOLS_SHADER)
+    if(NOT IOS AND NOT VISIONOS AND NOT ANDROID)
+        foreach(SHADER ${SHADERS})
+            add_bgfx_shader(${SHADER} "")
+        endforeach()
+    endif()
+    add_dependencies(Canvas shaderc)
+endif()
+
 set_property(TARGET Canvas PROPERTY FOLDER Polyfills)
 source_group(TREE ${CMAKE_CURRENT_SOURCE_DIR} FILES ${SOURCES})
 source_group("3rd party Sources" ${CMAKE_CURRENT_SOURCE_DIR} FILES ${FONT_SOURCES} ${NANOVG_SOURCES} ${ATLAS_SOURCES})
+source_group("Shaders" ${CMAKE_CURRENT_SOURCE_DIR} FILES ${SHADERS})
 target_compile_definitions(Canvas PRIVATE _CRT_SECURE_NO_WARNINGS)

.github/jobs/linux.yml
@@ -22,7 +22,7 @@ jobs:
 
     - script: |
         sudo apt-get update
-        sudo apt-get install libjavascriptcoregtk-4.0-dev libgl1-mesa-dev libcurl4-openssl-dev
+        sudo apt-get install libjavascriptcoregtk-4.0-dev libgl1-mesa-dev libcurl4-openssl-dev libwayland-dev
       displayName: 'Install packages'
 
     - script: |

commit 9379cf3c41eedbf3001d4f1e3872ced9a04e818e

fix build regression (#1476)
Fix build/installation regression introduced with
https://github.com/BabylonJS/BabylonNative/pull/1472

Install/Install.cmake
@@ -84,6 +84,11 @@ if(TARGET AppRuntime)
     install_include_for_targets(AppRuntime)
 endif()
 
+if(TARGET Scheduling)
+    install_targets(Scheduling)
+    install_include_for_targets(Scheduling)
+endif()
+
 if(TARGET ScriptLoader)
     install_targets(ScriptLoader)
     install_include_for_targets(ScriptLoader)

.github/jobs/test_install_linux.yml
@@ -23,7 +23,7 @@ jobs:
 
     - script: |
         sudo apt-get update
-        sudo apt-get install libjavascriptcoregtk-4.0-dev libgl1-mesa-dev libcurl4-openssl-dev
+        sudo apt-get install libjavascriptcoregtk-4.0-dev libgl1-mesa-dev libcurl4-openssl-dev libwayland-dev
       displayName: 'Install packages'
 
     - script: |

Install/Test/CMakeLists.txt
@@ -166,6 +166,7 @@ target_link_libraries(TestInstall
     openxr_loader
     OSDependent
     ScriptLoader
+    Scheduling
     spirv-cross-core
     spirv-cross-hlsl
     SPIRV