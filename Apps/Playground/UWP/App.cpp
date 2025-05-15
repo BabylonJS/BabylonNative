50657ea1ebe0dc23fd7ab27ee7eb4f45f3e23093
Andy Fillebrown <andy@docEdub.com>
2025-05-15 14:56:47 -0400

    Remove OpenXR dependencies

--- Apps/Playground/UWP/App.cpp
+++ Apps/Playground/UWP/App.cpp
@@ -4,7 +4,6 @@
 #include <Babylon/ScriptLoader.h>
 #include <Babylon/Plugins/NativeEngine.h>
 #include <Babylon/Plugins/NativeOptimizations.h>
-#include <Babylon/Plugins/NativeXr.h>
 #include <Babylon/Polyfills/Console.h>
 #include <Babylon/Polyfills/Window.h>
 #include <Babylon/Polyfills/XMLHttpRequest.h>
