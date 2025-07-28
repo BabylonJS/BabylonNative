f53c2ec4aa4f3f9b2094d6055d57817a1e5b6db1
Cedric Guillemet <1312968+CedricGuillemet@users.noreply.github.com>
2025-07-28 16:19:24 +0200

    opengl rendering type

--- Core/Graphics/Source/DeviceImpl_OpenGL.cpp
+++ Core/Graphics/Source/DeviceImpl_OpenGL.cpp
@@ -3,8 +3,11 @@
 
 namespace Babylon::Graphics
 {
+#ifdef ANDROID
     const bgfx::RendererType::Enum DeviceImpl::s_bgfxRenderType = bgfx::RendererType::OpenGLES;
-
+#else 
+    const bgfx::RendererType::Enum DeviceImpl::s_bgfxRenderType = bgfx::RendererType::OpenGL;
+#endif
     PlatformInfo DeviceImpl::GetPlatformInfo() const
     {
         return {static_cast<DeviceT>(bgfx::getInternalData()->context)};
