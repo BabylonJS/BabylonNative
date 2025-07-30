ece2441f0164735e5bd8f70ce26e0c1f38196026
Cedric Guillemet <1312968+CedricGuillemet@users.noreply.github.com>
2025-07-30 11:25:33 +0200

    xvfb-run

--- Core/Graphics/Source/DeviceImpl_OpenGL.cpp
+++ Core/Graphics/Source/DeviceImpl_OpenGL.cpp
@@ -3,11 +3,8 @@
 
 namespace Babylon::Graphics
 {
-#ifdef ANDROID
     const bgfx::RendererType::Enum DeviceImpl::s_bgfxRenderType = bgfx::RendererType::OpenGLES;
-#else 
-    const bgfx::RendererType::Enum DeviceImpl::s_bgfxRenderType = bgfx::RendererType::OpenGL;
-#endif
+
     PlatformInfo DeviceImpl::GetPlatformInfo() const
     {
         return {static_cast<DeviceT>(bgfx::getInternalData()->context)};
