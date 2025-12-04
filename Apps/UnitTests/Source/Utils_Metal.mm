e5363cdab50460484ac10535c51b259934a8095e
Gary Hsu <bghgary@users.noreply.github.com>
2025-12-03 17:00:06 -0800

    Add tests

--- Apps/UnitTests/Source/Utils_Metal.mm
+++ Apps/UnitTests/Source/Utils_Metal.mm
@@ -2,13 +2,19 @@
 #include "Utils.h"
 #import <Metal/Metal.h>
 
-Babylon::Graphics::TextureT CreateTestTexture(Babylon::Graphics::DeviceT device, uint32_t width, uint32_t height)
+Babylon::Graphics::TextureT CreateTestTexture(Babylon::Graphics::DeviceT device, uint32_t width, uint32_t height, uint32_t arraySize)
 {
     MTLTextureDescriptor* descriptor = [MTLTextureDescriptor
         texture2DDescriptorWithPixelFormat:MTLPixelFormatBGRA8Unorm
             width:width
             height:height
             mipmapped:NO];
+
+    if (arraySize > 1)
+    {
+        descriptor.textureType = MTLTextureType2DArray;
+        descriptor.arrayLength = arraySize;
+    }
 
     id<MTLTexture> texture = [device newTextureWithDescriptor:descriptor];
     EXPECT_NE(texture, nil);
