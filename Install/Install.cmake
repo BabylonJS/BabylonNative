671da83347fdc186fe4ed6595278a2affb683b36
Gary Hsu <bghgary@users.noreply.github.com>
2026-02-18 12:28:45 -0800

    Fix install cmake to include ShaderTool (#1607)

--- Install/Install.cmake
+++ Install/Install.cmake
@@ -4,7 +4,7 @@ if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
   set(CMAKE_INSTALL_PREFIX "${CMAKE_BINARY_DIR}/install" CACHE PATH "..." FORCE)
 endif()
 
-function(install_targets)
+function(install_lib)
     foreach(target IN LISTS ARGN)
         get_target_property(target_type ${target} TYPE)
         if(NOT target_type STREQUAL "INTERFACE_LIBRARY")
@@ -17,7 +17,14 @@ function(install_targets)
     endforeach()
 endfunction()
 
-function(install_include_for_targets)
+function(install_bin)
+    foreach(target IN LISTS ARGN)
+        install(FILES "$<TARGET_FILE:${target}>" DESTINATION bin)
+        install(FILES "$<TARGET_FILE_DIR:${target}>/$<TARGET_FILE_PREFIX:${target}>$<TARGET_FILE_BASE_NAME:${target}>.pdb" DESTINATION bin OPTIONAL)
+    endforeach()
+endfunction()
+
+function(install_include)
     foreach(target IN LISTS ARGN)
         get_target_property(target_interface_include_directories ${target} INTERFACE_INCLUDE_DIRECTORIES)
         foreach(include_directory IN LISTS target_interface_include_directories)
@@ -33,77 +40,77 @@ endfunction()
 # ----------------
 
 ## arcana.cpp
-install_targets(arcana)
+install_lib(arcana)
 
 ## bgfx
-install_targets(bimg_encode bimg_decode bgfx bimg bx minz)
+install_lib(bimg_encode bimg_decode bgfx bimg bx minz)
 
 ## glslang
-install_targets(GenericCodeGen glslang glslang-default-resource-limits MachineIndependent OGLCompiler OSDependent SPIRV)
+install_lib(GenericCodeGen glslang glslang-default-resource-limits MachineIndependent OGLCompiler OSDependent SPIRV)
 
 ## libwebp
 if(TARGET webp)
-    install_targets(webp)
+    install_lib(webp)
 endif()
 
 ## SPIRV-Cross
-install_targets(spirv-cross-core)
+install_lib(spirv-cross-core)
 if(TARGET spirv-cross-glsl)
-    install_targets(spirv-cross-glsl)
+    install_lib(spirv-cross-glsl)
 endif()
 if(TARGET spirv-cross-msl)
-    install_targets(spirv-cross-msl)
+    install_lib(spirv-cross-msl)
 endif()
 if(TARGET spirv-cross-hlsl)
-    install_targets(spirv-cross-hlsl)
+    install_lib(spirv-cross-hlsl)
 endif()
 
 ## XR
 if(TARGET openxr_loader)
-    install_targets(openxr_loader)
+    install_lib(openxr_loader)
 endif()
 
 if(TARGET xr)
-    install_targets(xr)
+    install_lib(xr)
 endif()
 
 ## UrlLib
-install_targets(UrlLib)
+install_lib(UrlLib)
 
 # ----------------
 # Core
 # ----------------
 
-install_targets(Foundation)
-install_include_for_targets(Foundation)
+install_lib(Foundation)
+install_include(Foundation)
 
-install_targets(JsRuntime)
-install_include_for_targets(JsRuntime)
+install_lib(JsRuntime)
+install_include(JsRuntime)
 
 # Note libs are in the `Graphics` target but includes are in `GraphicsDevice` target
-install_targets(Graphics)
-install_include_for_targets(GraphicsDevice)
+install_lib(Graphics)
+install_include(GraphicsDevice)
 
 if(TARGET AppRuntime)
-    install_targets(AppRuntime)
-    install_include_for_targets(AppRuntime)
+    install_lib(AppRuntime)
+    install_include(AppRuntime)
 endif()
 
 if(TARGET Scheduling)
-    install_targets(Scheduling)
-    install_include_for_targets(Scheduling)
+    install_lib(Scheduling)
+    install_include(Scheduling)
 endif()
 
 if(TARGET ScriptLoader)
-    install_targets(ScriptLoader)
-    install_include_for_targets(ScriptLoader)
+    install_lib(ScriptLoader)
+    install_include(ScriptLoader)
 endif()
 
-install_targets(napi)
-install_include_for_targets(napi)
+install_lib(napi)
+install_include(napi)
 
 if(NAPI_JAVASCRIPT_ENGINE STREQUAL "V8" AND JSRUNTIMEHOST_CORE_APPRUNTIME_V8_INSPECTOR)
-    install_targets(llhttp_static v8inspector)
+    install_lib(llhttp_static v8inspector)
 endif()
 
 # Manually install the JSI headers
@@ -118,65 +125,69 @@ if(NAPI_JAVASCRIPT_ENGINE STREQUAL "JSI")
     install(DIRECTORY ${V8JSI_PACKAGE_PATH}/build/native/jsi/jsi TYPE INCLUDE)
 endif()
 
-install_targets(napi-extensions)
-install_include_for_targets(napi-extensions)
+install_lib(napi-extensions)
+install_include(napi-extensions)
 
 # ----------------
 # Plugins
 # ----------------
 
 if(TARGET ExternalTexture)
-    install_targets(ExternalTexture)
-    install_include_for_targets(ExternalTexture)
+    install_lib(ExternalTexture)
+    install_include(ExternalTexture)
 endif()
 
 if(TARGET NativeCamera)
-    install_targets(NativeCamera)
-    install_include_for_targets(NativeCamera)
+    install_lib(NativeCamera)
+    install_include(NativeCamera)
 endif()
 
 if(TARGET NativeCapture)
-    install_targets(NativeCapture)
-    install_include_for_targets(NativeCapture)
+    install_lib(NativeCapture)
+    install_include(NativeCapture)
 endif()
 
 if(TARGET NativeEngine)
-    install_targets(NativeEngine)
-    install_include_for_targets(NativeEngine)
+    install_lib(NativeEngine)
+    install_include(NativeEngine)
 endif()
 
 if(TARGET NativeInput)
-    install_targets(NativeInput)
-    install_include_for_targets(NativeInput)
+    install_lib(NativeInput)
+    install_include(NativeInput)
 endif()
 
 if(TARGET NativeOptimizations)
-    install_targets(NativeOptimizations)
-    install_include_for_targets(NativeOptimizations)
+    install_lib(NativeOptimizations)
+    install_include(NativeOptimizations)
 endif()
 
 if(TARGET NativeTracing)
-    install_targets(NativeTracing)
-    install_include_for_targets(NativeTracing)
+    install_lib(NativeTracing)
+    install_include(NativeTracing)
 endif()
 
 if(TARGET NativeXr)
-    install_targets(NativeXr)
-    install_include_for_targets(NativeXr)
+    install_lib(NativeXr)
+    install_include(NativeXr)
 endif()
 
 if(TARGET NativeEncoding)
-    install_targets(NativeEncoding)
-    install_include_for_targets(NativeEncoding)
+    install_lib(NativeEncoding)
+    install_include(NativeEncoding)
 endif()
 
 if(TARGET ShaderCache)
-    install_targets(ShaderCache)
-    install_include_for_targets(ShaderCache)
+    install_lib(ShaderCache)
+    install_include(ShaderCache)
 endif()
 
 if(TARGET ShaderCompiler)
-    install_targets(ShaderCompiler)
+    install_lib(ShaderCompiler)
+endif()
+
+if(TARGET ShaderTool)
+    install_bin(ShaderTool)
 endif()
 
 # ----------------
@@ -184,46 +195,46 @@ endif()
 # ----------------
 
 if(TARGET Canvas)
-    install_targets(Canvas)
-    install_include_for_targets(Canvas)
+    install_lib(Canvas)
+    install_include(Canvas)
 endif()
 
 if(TARGET Console)
-    install_targets(Console)
-    install_include_for_targets(Console)
+    install_lib(Console)
+    install_include(Console)
 endif()
 
 if(TARGET Window)
-    install_targets(Window)
-    install_include_for_targets(Window)
+    install_lib(Window)
+    install_include(Window)
 endif()
 
 if(TARGET XMLHttpRequest)
-    install_targets(XMLHttpRequest)
-    install_include_for_targets(XMLHttpRequest)
+    install_lib(XMLHttpRequest)
+    install_include(XMLHttpRequest)
 endif()
 
 if(TARGET Blob)
-    install_targets(Blob)
-    install_include_for_targets(Blob)
+    install_lib(Blob)
+    install_include(Blob)
 endif()
 
 if(TARGET URL)
-    install_targets(URL)
-    install_include_for_targets(URL)
+    install_lib(URL)
+    install_include(URL)
 endif()
 
 if(TARGET AbortController)
-    install_targets(AbortController)
-    install_include_for_targets(AbortController)
+    install_lib(AbortController)
+    install_include(AbortController)
 endif()
 
 if(TARGET WebSocket)
-    install_targets(WebSocket)
-    install_include_for_targets(WebSocket)
+    install_lib(WebSocket)
+    install_include(WebSocket)
 endif()
 
 if(TARGET Performance)
-    install_targets(Performance)
-    install_include_for_targets(Performance)
+    install_lib(Performance)
+    install_include(Performance)
 endif()
