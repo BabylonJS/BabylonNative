e6d648175ac764bac174b60fc86e682cd7dd1e63
Sergio Ricardo Zerbetto Masson <sergioze@microsoft.com>
2026-02-25 13:58:56 -0300

    Updated UWP Playground to C++/WinRT instead of C++/CX for C++20 compatibility

--- Apps/Playground/UWP/App.cpp
+++ Apps/Playground/UWP/App.cpp
@@ -1,42 +1,22 @@
 #include "App.h"
 
-#include <winrt/windows.ui.core.h>
-
 #include <sstream>
 #include <iostream>
 
-namespace
-{
-    template <typename T>
-    T from_cx(Platform::Object^ from)
-    {
-        T to{ nullptr };
-
-        if (from != nullptr)
-        {
-            winrt::check_hresult(reinterpret_cast<::IUnknown*>(from)
-                ->QueryInterface(winrt::guid_of<T>(), winrt::put_abi(to)));
-        }
-
-        return to;
-    }
-}
-
-using namespace Windows::ApplicationModel;
-using namespace Windows::ApplicationModel::Core;
-using namespace Windows::ApplicationModel::Activation;
-using namespace Windows::UI::Core;
-using namespace Windows::UI::Input;
-using namespace Windows::System;
-using namespace Windows::Foundation;
-using namespace Windows::Graphics::Display;
+using namespace winrt::Windows::ApplicationModel;
+using namespace winrt::Windows::ApplicationModel::Core;
+using namespace winrt::Windows::ApplicationModel::Activation;
+using namespace winrt::Windows::UI::Core;
+using namespace winrt::Windows::UI::Input;
+using namespace winrt::Windows::System;
+using namespace winrt::Windows::Foundation;
+using namespace winrt::Windows::Graphics::Display;
 
 // The main function is only used to initialize our IFrameworkView class.
-[Platform::MTAThread]
-int main(Platform::Array<Platform::String^>^)
+int __stdcall wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
 {
-    auto direct3DApplicationSource = ref new Direct3DApplicationSource();
-    CoreApplication::Run(direct3DApplicationSource);
+    winrt::init_apartment();
+    CoreApplication::Run(winrt::make<Direct3DApplicationSource>());
     return 0;
 }
 
@@ -66,73 +46,59 @@ void ProcessMouseButtons(Babylon::Plugins::NativeInput* input, PointerUpdateKind
     }
 }
 
-IFrameworkView^ Direct3DApplicationSource::CreateView()
+IFrameworkView Direct3DApplicationSource::CreateView()
 {
-    return ref new App();
+    return winrt::make<App>();
 }
 
 App::App() :
-    m_windowClosed{ false },
-    m_windowVisible{ true }
+    m_windowClosed{false},
+    m_windowVisible{true}
 {
 }
 
 // The first method called when the IFrameworkView is being created.
-void App::Initialize(CoreApplicationView^ applicationView)
+void App::Initialize(CoreApplicationView const& applicationView)
 {
     // Register event handlers for app lifecycle. This example includes Activated, so that we
     // can make the CoreWindow active and start rendering on the window.
-    applicationView->Activated +=
-        ref new TypedEventHandler<CoreApplicationView^, IActivatedEventArgs^>(this, &App::OnActivated);
+    applicationView.Activated({this, &App::OnActivated});
 
-    CoreApplication::Suspending +=
-        ref new EventHandler<SuspendingEventArgs^>(this, &App::OnSuspending);
+    CoreApplication::Suspending({this, &App::OnSuspending});
 
-    CoreApplication::Resuming +=
-        ref new EventHandler<Platform::Object^>(this, &App::OnResuming);
+    CoreApplication::Resuming({this, &App::OnResuming});
 }
 
 // Called when the CoreWindow object is created (or re-created).
-void App::SetWindow(CoreWindow^ window)
+void App::SetWindow(CoreWindow const& window)
 {
-    window->SizeChanged +=
-        ref new TypedEventHandler<CoreWindow^, WindowSizeChangedEventArgs^>(this, &App::OnWindowSizeChanged);
+    window.SizeChanged({this, &App::OnWindowSizeChanged});
 
-    window->VisibilityChanged +=
-        ref new TypedEventHandler<CoreWindow^, VisibilityChangedEventArgs^>(this, &App::OnVisibilityChanged);
+    window.VisibilityChanged({this, &App::OnVisibilityChanged});
 
-    window->Closed +=
-        ref new TypedEventHandler<CoreWindow^, CoreWindowEventArgs^>(this, &App::OnWindowClosed);
+    window.Closed({this, &App::OnWindowClosed});
 
-    DisplayInformation^ currentDisplayInformation = DisplayInformation::GetForCurrentView();
+    DisplayInformation currentDisplayInformation = DisplayInformation::GetForCurrentView();
 
-    currentDisplayInformation->DpiChanged +=
-        ref new TypedEventHandler<DisplayInformation^, Object^>(this, &App::OnDpiChanged);
+    currentDisplayInformation.DpiChanged({this, &App::OnDpiChanged});
 
-    currentDisplayInformation->OrientationChanged +=
-        ref new TypedEventHandler<DisplayInformation^, Object^>(this, &App::OnOrientationChanged);
+    currentDisplayInformation.OrientationChanged({this, &App::OnOrientationChanged});
 
-    DisplayInformation::DisplayContentsInvalidated +=
-        ref new TypedEventHandler<DisplayInformation^, Object^>(this, &App::OnDisplayContentsInvalidated);
+    DisplayInformation::DisplayContentsInvalidated({this, &App::OnDisplayContentsInvalidated});
 
-    window->PointerMoved +=
-        ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &App::OnPointerMoved);
+    window.PointerMoved({this, &App::OnPointerMoved});
 
-    window->PointerPressed +=
-        ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &App::OnPointerPressed);
+    window.PointerPressed({this, &App::OnPointerPressed});
 
-    window->PointerReleased +=
-        ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &App::OnPointerReleased);
+    window.PointerReleased({this, &App::OnPointerReleased});
 
-    window->PointerWheelChanged +=
-        ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &App::OnPointerWheelChanged);
+    window.PointerWheelChanged({this, &App::OnPointerWheelChanged});
 
-    window->KeyDown +=
-        ref new TypedEventHandler<CoreWindow^, KeyEventArgs^>(this, &App::OnKeyPressed);
+    window.KeyDown({this, &App::OnKeyPressed});
 }
 
 // Initializes scene resources, or loads a previously saved app state.
-void App::Load(Platform::String^ entryPoint)
+void App::Load(winrt::hstring const& /*entryPoint*/)
 {
 }
 
@@ -149,7 +115,7 @@ void App::Run()
             m_appContext->DeviceUpdate().Start();
         }
 
-        CoreWindow::GetForCurrentThread()->Dispatcher->ProcessEvents(CoreProcessEventsOption::ProcessAllIfPresent);
+        CoreWindow::GetForCurrentThread().Dispatcher().ProcessEvents(CoreProcessEventsOption::ProcessAllIfPresent);
     }
 }
 
@@ -163,30 +129,30 @@ void App::Uninitialize()
 
 // Application lifecycle event handlers.
 
-void App::OnActivated(CoreApplicationView^ applicationView, IActivatedEventArgs^ args)
+void App::OnActivated(CoreApplicationView const& applicationView, IActivatedEventArgs const& args)
 {
     // Run() won't start until the CoreWindow is activated.
-    CoreWindow::GetForCurrentThread()->Activate();
+    CoreWindow::GetForCurrentThread().Activate();
 
-    if (args->Kind == Activation::ActivationKind::File)
+    if (args.Kind() == ActivationKind::File)
     {
-        m_files = static_cast<FileActivatedEventArgs^>(args)->Files;
+        m_files = args.as<FileActivatedEventArgs>().Files();
     }
     else
     {
         m_files = nullptr;
     }
 
-    RestartRuntime(applicationView->CoreWindow->Bounds);
+    RestartRuntime(applicationView.CoreWindow().Bounds());
 }
 
-void App::OnSuspending(Platform::Object^ sender, SuspendingEventArgs^ args)
+void App::OnSuspending(IInspectable const& /*sender*/, SuspendingEventArgs const& args)
 {
     // Save app state after requesting a deferral. Holding a deferral
     // indicates that the application is busy performing suspending operations. Be
     // aware that a deferral may not be held indefinitely. After about five seconds,
     // the app will be forced to exit.
-    auto deferral = args->SuspendingOperation->GetDeferral();
+    auto deferral = args.SuspendingOperation().GetDeferral();
 
     if (m_appContext)
     {
@@ -196,10 +162,10 @@ void App::OnSuspending(Platform::Object^ sender, SuspendingEventArgs^ args)
         m_appContext->Runtime().Suspend();
     }
 
-    deferral->Complete();
+    deferral.Complete();
 }
 
-void App::OnResuming(Platform::Object^ sender, Platform::Object^ args)
+void App::OnResuming(IInspectable const& /*sender*/, IInspectable const& /*args*/)
 {
     if (m_appContext)
     {
@@ -215,43 +181,43 @@ void App::OnResuming(Platform::Object^ sender, Platform::Object^ args)
 
 // Window event handlers.
 
-void App::OnWindowSizeChanged(CoreWindow^ /*sender*/, WindowSizeChangedEventArgs^ args)
+void App::OnWindowSizeChanged(CoreWindow const& /*sender*/, WindowSizeChangedEventArgs const& args)
 {
     if (m_appContext)
     {
-        size_t width = static_cast<size_t>(args->Size.Width * m_displayScale);
-        size_t height = static_cast<size_t>(args->Size.Height * m_displayScale);
+        size_t width = static_cast<size_t>(args.Size().Width * m_displayScale);
+        size_t height = static_cast<size_t>(args.Size().Height * m_displayScale);
         m_appContext->Device().UpdateSize(width, height);
     }
 }
 
-void App::OnVisibilityChanged(CoreWindow^ sender, VisibilityChangedEventArgs^ args)
+void App::OnVisibilityChanged(CoreWindow const& /*sender*/, VisibilityChangedEventArgs const& args)
 {
-    m_windowVisible = args->Visible;
+    m_windowVisible = args.Visible();
 }
 
-void App::OnWindowClosed(CoreWindow^ sender, CoreWindowEventArgs^ args)
+void App::OnWindowClosed(CoreWindow const& /*sender*/, CoreWindowEventArgs const& /*args*/)
 {
     m_windowClosed = true;
     Uninitialize();
 }
 
-void App::OnPointerMoved(CoreWindow^, PointerEventArgs^ args)
+void App::OnPointerMoved(CoreWindow const&, PointerEventArgs const& args)
 {
     if (m_appContext && m_appContext->Input())
     {
-        const auto& position = args->CurrentPoint->RawPosition;
-        const auto deviceType = args->CurrentPoint->PointerDevice->PointerDeviceType;
-        const auto deviceSlot = args->CurrentPoint->PointerId;
-        const auto updateKind = args->CurrentPoint->Properties->PointerUpdateKind;
+        const auto position = args.CurrentPoint().RawPosition();
+        const auto deviceType = args.CurrentPoint().PointerDevice().PointerDeviceType();
+        const auto deviceSlot = args.CurrentPoint().PointerId();
+        const auto updateKind = args.CurrentPoint().Properties().PointerUpdateKind();
         const auto x = static_cast<int>(position.X * m_displayScale);
         const auto y = static_cast<int>(position.Y * m_displayScale);
 
-        if (deviceType == Windows::Devices::Input::PointerDeviceType::Mouse)
+        if (deviceType == winrt::Windows::Devices::Input::PointerDeviceType::Mouse)
         {
             m_appContext->Input()->MouseMove(x, y);
 
-            if (args->CurrentPoint->IsInContact)
+            if (args.CurrentPoint().IsInContact())
             {
                 ProcessMouseButtons(m_appContext->Input(), updateKind, x, y);
             }
@@ -263,18 +229,18 @@ void App::OnPointerMoved(CoreWindow^, PointerEventArgs^ args)
     }
 }
 
-void App::OnPointerPressed(CoreWindow^, PointerEventArgs^ args)
+void App::OnPointerPressed(CoreWindow const&, PointerEventArgs const& args)
 {
     if (m_appContext && m_appContext->Input())
     {
-        const auto& position = args->CurrentPoint->RawPosition;
-        const auto deviceType = args->CurrentPoint->PointerDevice->PointerDeviceType;
-        const auto deviceSlot = args->CurrentPoint->PointerId;
-        const auto updateKind = args->CurrentPoint->Properties->PointerUpdateKind;
+        const auto position = args.CurrentPoint().RawPosition();
+        const auto deviceType = args.CurrentPoint().PointerDevice().PointerDeviceType();
+        const auto deviceSlot = args.CurrentPoint().PointerId();
+        const auto updateKind = args.CurrentPoint().Properties().PointerUpdateKind();
         const auto x = static_cast<int>(position.X * m_displayScale);
         const auto y = static_cast<int>(position.Y * m_displayScale);
 
-        if (deviceType == Windows::Devices::Input::PointerDeviceType::Mouse)
+        if (deviceType == winrt::Windows::Devices::Input::PointerDeviceType::Mouse)
         {
             ProcessMouseButtons(m_appContext->Input(), updateKind, x, y);
         }
@@ -285,18 +251,18 @@ void App::OnPointerPressed(CoreWindow^, PointerEventArgs^ args)
     }
 }
 
-void App::OnPointerReleased(CoreWindow^, PointerEventArgs^ args)
+void App::OnPointerReleased(CoreWindow const&, PointerEventArgs const& args)
 {
     if (m_appContext && m_appContext->Input())
     {
-        const auto& position = args->CurrentPoint->RawPosition;
-        const auto deviceType = args->CurrentPoint->PointerDevice->PointerDeviceType;
-        const auto deviceSlot = args->CurrentPoint->PointerId;
-        const auto updateKind = args->CurrentPoint->Properties->PointerUpdateKind;
+        const auto position = args.CurrentPoint().RawPosition();
+        const auto deviceType = args.CurrentPoint().PointerDevice().PointerDeviceType();
+        const auto deviceSlot = args.CurrentPoint().PointerId();
+        const auto updateKind = args.CurrentPoint().Properties().PointerUpdateKind();
         const auto x = static_cast<int>(position.X * m_displayScale);
         const auto y = static_cast<int>(position.Y * m_displayScale);
 
-        if (deviceType == Windows::Devices::Input::PointerDeviceType::Mouse)
+        if (deviceType == winrt::Windows::Devices::Input::PointerDeviceType::Mouse)
         {
             ProcessMouseButtons(m_appContext->Input(), updateKind, x, y);
         }
@@ -306,53 +272,54 @@ void App::OnPointerReleased(CoreWindow^, PointerEventArgs^ args)
         }
     }
 }
-void App::OnPointerWheelChanged(CoreWindow^, PointerEventArgs^ args)
+
+void App::OnPointerWheelChanged(CoreWindow const&, PointerEventArgs const& args)
 {
     if (m_appContext && m_appContext->Input())
     {
-        const auto delta = args->CurrentPoint->Properties->MouseWheelDelta;
+        const auto delta = args.CurrentPoint().Properties().MouseWheelDelta();
         m_appContext->Input()->MouseWheel(Babylon::Plugins::NativeInput::MOUSEWHEEL_Y_ID, -delta);
     }
 }
 
-void App::OnKeyPressed(CoreWindow^ window, KeyEventArgs^ args)
+void App::OnKeyPressed(CoreWindow const& window, KeyEventArgs const& args)
 {
-    if (args->VirtualKey == VirtualKey::R)
+    if (args.VirtualKey() == VirtualKey::R)
     {
-        RestartRuntime(window->Bounds);
+        RestartRuntime(window.Bounds());
     }
 }
 
 // DisplayInformation event handlers.
 
-void App::OnDpiChanged(DisplayInformation^ /*sender*/, Object^ /*args*/)
+void App::OnDpiChanged(DisplayInformation const& /*sender*/, IInspectable const& /*args*/)
 {
-    DisplayInformation^ displayInformation = DisplayInformation::GetForCurrentView();
-    m_displayScale = static_cast<float>(displayInformation->RawPixelsPerViewPixel);
+    DisplayInformation displayInformation = DisplayInformation::GetForCurrentView();
+    m_displayScale = static_cast<float>(displayInformation.RawPixelsPerViewPixel());
     // resize event happens after. No need to force resize here.
 }
 
-void App::OnOrientationChanged(DisplayInformation^ sender, Object^ args)
+void App::OnOrientationChanged(DisplayInformation const& /*sender*/, IInspectable const& /*args*/)
 {
     // TODO: Implement.
     //m_deviceResources->SetCurrentOrientation(sender->CurrentOrientation);
 }
 
-void App::OnDisplayContentsInvalidated(DisplayInformation^ sender, Object^ args)
+void App::OnDisplayContentsInvalidated(DisplayInformation const& /*sender*/, IInspectable const& /*args*/)
 {
     // TODO: Implement.
     //m_deviceResources->ValidateDevice();
 }
 
-void App::RestartRuntime(Windows::Foundation::Rect bounds)
+void App::RestartRuntime(Rect bounds)
 {
     Uninitialize();
 
-    DisplayInformation^ displayInformation = DisplayInformation::GetForCurrentView();
-    m_displayScale = static_cast<float>(displayInformation->RawPixelsPerViewPixel);
+    DisplayInformation displayInformation = DisplayInformation::GetForCurrentView();
+    m_displayScale = static_cast<float>(displayInformation.RawPixelsPerViewPixel());
     size_t width = static_cast<size_t>(bounds.Width * m_displayScale);
     size_t height = static_cast<size_t>(bounds.Height * m_displayScale);
-    auto window = from_cx<winrt::Windows::Foundation::IInspectable>(CoreWindow::GetForCurrentThread());
+    IInspectable window{CoreWindow::GetForCurrentThread()};
 
     m_appContext.emplace(
         window,
@@ -371,13 +338,13 @@ void App::RestartRuntime(Windows::Foundation::Rect bounds)
     }
     else
     {
-        for (unsigned int idx = 0; idx < m_files->Size; idx++)
+        for (uint32_t idx = 0; idx < m_files.Size(); idx++)
         {
-            auto file{static_cast<Windows::Storage::IStorageFile^>(m_files->GetAt(idx))};
+            auto file = m_files.GetAt(idx).as<winrt::Windows::Storage::IStorageFile>();
 
             // There is no built-in way to convert a local file path to a url in UWP, but
             // Foundation::Uri works with a url constructed using "file:///" with a local path.
-            m_appContext->ScriptLoader().LoadScript("file:///" + winrt::to_string(file->Path->Data()));
+            m_appContext->ScriptLoader().LoadScript("file:///" + winrt::to_string(file.Path()));
         }
 
         m_appContext->ScriptLoader().LoadScript("app:///Scripts/playground_runner.js");
--- Apps/Playground/UWP/App.h
+++ Apps/Playground/UWP/App.h
@@ -3,52 +3,62 @@
 #include <Shared/AppContext.h>
 #include <optional>
 
+#include <winrt/Windows.ApplicationModel.h>
+#include <winrt/Windows.ApplicationModel.Activation.h>
+#include <winrt/Windows.ApplicationModel.Core.h>
+#include <winrt/Windows.Devices.Input.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Graphics.Display.h>
+#include <winrt/Windows.Storage.h>
+#include <winrt/Windows.System.h>
+#include <winrt/Windows.UI.Core.h>
+#include <winrt/Windows.UI.Input.h>
+
 // Main entry point for our app. Connects the app with the Windows shell and handles application lifecycle events.
-ref class App sealed : public Windows::ApplicationModel::Core::IFrameworkView
+struct App : winrt::implements<App, winrt::Windows::ApplicationModel::Core::IFrameworkView>
 {
-public:
     App();
 
     // IFrameworkView Methods.
-    virtual void Initialize(Windows::ApplicationModel::Core::CoreApplicationView^ applicationView);
-    virtual void SetWindow(Windows::UI::Core::CoreWindow^ window);
-    virtual void Load(Platform::String^ entryPoint);
-    virtual void Run();
-    virtual void Uninitialize();
+    void Initialize(winrt::Windows::ApplicationModel::Core::CoreApplicationView const& applicationView);
+    void SetWindow(winrt::Windows::UI::Core::CoreWindow const& window);
+    void Load(winrt::hstring const& entryPoint);
+    void Run();
+    void Uninitialize();
 
 private:
     // Application lifecycle event handlers.
-    void OnActivated(Windows::ApplicationModel::Core::CoreApplicationView^ applicationView, Windows::ApplicationModel::Activation::IActivatedEventArgs^ args);
-    void OnSuspending(Platform::Object^ sender, Windows::ApplicationModel::SuspendingEventArgs^ args);
-    void OnResuming(Platform::Object^ sender, Platform::Object^ args);
+    void OnActivated(winrt::Windows::ApplicationModel::Core::CoreApplicationView const& applicationView, winrt::Windows::ApplicationModel::Activation::IActivatedEventArgs const& args);
+    void OnSuspending(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::ApplicationModel::SuspendingEventArgs const& args);
+    void OnResuming(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::Foundation::IInspectable const& args);
 
     // Window event handlers.
-    void OnWindowSizeChanged(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::WindowSizeChangedEventArgs^ args);
-    void OnVisibilityChanged(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::VisibilityChangedEventArgs^ args);
-    void OnWindowClosed(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::CoreWindowEventArgs^ args);
-    void OnPointerMoved(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::PointerEventArgs^ args);
-    void OnPointerPressed(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::PointerEventArgs^ args);
-    void OnPointerReleased(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::PointerEventArgs^ args);
-    void OnPointerWheelChanged(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::PointerEventArgs ^ args);
-    void OnKeyPressed(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::KeyEventArgs^ args);
+    void OnWindowSizeChanged(winrt::Windows::UI::Core::CoreWindow const& sender, winrt::Windows::UI::Core::WindowSizeChangedEventArgs const& args);
+    void OnVisibilityChanged(winrt::Windows::UI::Core::CoreWindow const& sender, winrt::Windows::UI::Core::VisibilityChangedEventArgs const& args);
+    void OnWindowClosed(winrt::Windows::UI::Core::CoreWindow const& sender, winrt::Windows::UI::Core::CoreWindowEventArgs const& args);
+    void OnPointerMoved(winrt::Windows::UI::Core::CoreWindow const& sender, winrt::Windows::UI::Core::PointerEventArgs const& args);
+    void OnPointerPressed(winrt::Windows::UI::Core::CoreWindow const& sender, winrt::Windows::UI::Core::PointerEventArgs const& args);
+    void OnPointerReleased(winrt::Windows::UI::Core::CoreWindow const& sender, winrt::Windows::UI::Core::PointerEventArgs const& args);
+    void OnPointerWheelChanged(winrt::Windows::UI::Core::CoreWindow const& sender, winrt::Windows::UI::Core::PointerEventArgs const& args);
+    void OnKeyPressed(winrt::Windows::UI::Core::CoreWindow const& sender, winrt::Windows::UI::Core::KeyEventArgs const& args);
 
     // DisplayInformation event handlers.
-    void OnDpiChanged(Windows::Graphics::Display::DisplayInformation^ sender, Platform::Object^ args);
-    void OnOrientationChanged(Windows::Graphics::Display::DisplayInformation^ sender, Platform::Object^ args);
-    void OnDisplayContentsInvalidated(Windows::Graphics::Display::DisplayInformation^ sender, Platform::Object^ args);
+    void OnDpiChanged(winrt::Windows::Graphics::Display::DisplayInformation const& sender, winrt::Windows::Foundation::IInspectable const& args);
+    void OnOrientationChanged(winrt::Windows::Graphics::Display::DisplayInformation const& sender, winrt::Windows::Foundation::IInspectable const& args);
+    void OnDisplayContentsInvalidated(winrt::Windows::Graphics::Display::DisplayInformation const& sender, winrt::Windows::Foundation::IInspectable const& args);
 
-    void RestartRuntime(Windows::Foundation::Rect bounds);
+    void RestartRuntime(winrt::Windows::Foundation::Rect bounds);
 
     std::optional<AppContext> m_appContext{};
 
-    Windows::Foundation::Collections::IVectorView<Windows::Storage::IStorageItem^>^ m_files;
+    winrt::Windows::Foundation::Collections::IVectorView<winrt::Windows::Storage::IStorageItem> m_files{nullptr};
     bool m_windowClosed;
     bool m_windowVisible;
-    float m_displayScale{ 1.f };
+    float m_displayScale{1.f};
 };
 
-ref class Direct3DApplicationSource sealed : Windows::ApplicationModel::Core::IFrameworkViewSource
+struct Direct3DApplicationSource : winrt::implements<Direct3DApplicationSource, winrt::Windows::ApplicationModel::Core::IFrameworkViewSource>
 {
-public:
-    virtual Windows::ApplicationModel::Core::IFrameworkView^ CreateView();
+    winrt::Windows::ApplicationModel::Core::IFrameworkView CreateView();
 };
